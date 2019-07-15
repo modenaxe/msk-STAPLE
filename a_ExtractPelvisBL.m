eb684acfa846863cba78a89db405c00c301321c7
Luca Modenese <l.modenese@imperial.ac.uk>
2019-07-15 16:26:42 +0100

    aesthetics in script

--- a_ExtractPelvisBL.m
+++ a_ExtractPelvisBL.m
@@ -25,20 +25,20 @@ InertiaInfo  = calcPrincInertiaAxes(MassInfo.Imat);
 
 % PointCloud = v_MRI;
 % InertiaInfo  = calcInertiaMatrix(PointCloud);
-coeff = -1;
+coeff = 1;
 v = transfMeshPointsRefSyst(v_MRI, MassInfo.COM, coeff*InertiaInfo.PrincAxes);
-
+v = v_MRI;
 % BL description
 Markers.LASIS = getBonyLandmark(pickPointsInOctant(v, 1),'max','z');
-Markers.LICT = getBonyLandmark(pickPointsInOctant(v, 1),'max','x');
+Markers.LICT  = getBonyLandmark(pickPointsInOctant(v, 1),'max','x');
 Markers.RASIS = getBonyLandmark(pickPointsInOctant(v, 2),'max','z');
-Markers.RICT = getBonyLandmark(pickPointsInOctant(v, 2),'min','x');
+Markers.RICT  = getBonyLandmark(pickPointsInOctant(v, 2),'min','x');
 Markers.LPSIS = getBonyLandmark(pickPointsInOctant(v, 5),'min','z');
 Markers.RPSIS = getBonyLandmark(pickPointsInOctant(v, 6),'min','z');
-Markers.RLPP = getBonyLandmark(pickPointsInOctant(v, 7),'min','y');
-Markers.RIIT = getBonyLandmark(pickPointsInOctant(v, 7),'min','z');
-Markers.LLPP = getBonyLandmark(pickPointsInOctant(v, 8),'min','y');
-Markers.LIIT = getBonyLandmark(pickPointsInOctant(v, 8),'min','z');
+Markers.RLPP  = getBonyLandmark(pickPointsInOctant(v, 7),'min','y');
+Markers.RIIT  = getBonyLandmark(pickPointsInOctant(v, 7),'min','z');
+Markers.LLPP  = getBonyLandmark(pickPointsInOctant(v, 8),'min','y');
+Markers.LIIT  = getBonyLandmark(pickPointsInOctant(v, 8),'min','z');
 
 % HJC estimation via regression equations
 if isfield(Markers,{'RASIS', 'LASIS','LPSIS', 'RPSIS'})
@@ -66,17 +66,17 @@ patch('Faces',f,'Vertices',v,'FaceColor',[0.8 0.8 0.8]);hold on;grid on
 % plot3(v(:,1),v(:,2),v(:,3),'.','Color',color_set{1});  
 plot_refsyst(gca, [0 0 0], eye(3), 100);
 % plot bony landmarks with label
-label_switch =1;
+label_switch =0;
 plotBL(Markers, label_switch);
 
 %============== TO DO FUNCTION =============
-Ffield = fields(Markers);
-fid = fopen('C:\Users\Luca M\Desktop\Package_for_test\Pelvis_BL.txt','w+');
-fprintf(fid, '%s\n', 'Time    0'); 
-Or_COM_in_InAxes = -((coeff*InertiaInfo.PrincAxes)*(InertiaInfo.COM)')';
-for n_m = 1:size(fields(Markers))
-    BL.colheaders(n_m) = {Ffield{n_m}};
-     BL.data(n_m,:) = transfMeshPointsRefSyst(Markers.(Ffield{n_m}), Or_COM_in_InAxes, coeff*InertiaInfo.PrincAxes');
-    fprintf(fid, '%s %f %f %f\n', BL.colheaders{n_m},   BL.data(n_m,:))
-end
-fclose all
+% Ffield = fields(Markers);
+% fid = fopen('C:\Users\Luca M\Desktop\Package_for_test\Pelvis_BL.txt','w+');
+% fprintf(fid, '%s\n', 'Time    0'); 
+% Or_COM_in_InAxes = -((coeff*InertiaInfo.PrincAxes)*(InertiaInfo.COM)')';
+% for n_m = 1:size(fields(Markers))
+%     BL.colheaders(n_m) = {Ffield{n_m}};
+%      BL.data(n_m,:) = transfMeshPointsRefSyst(Markers.(Ffield{n_m}), Or_COM_in_InAxes, coeff*InertiaInfo.PrincAxes');
+%     fprintf(fid, '%s %f %f %f\n', BL.colheaders{n_m},   BL.data(n_m,:))
+% end
+% fclose all
