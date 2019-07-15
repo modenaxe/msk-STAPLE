05d483eac181d5776bb2104a22de1b8611c373b2
Luca Modenese <l.modenese@imperial.ac.uk>
2019-07-15 15:54:55 +0100

    making two parts of bone cutted shorter, similar to examples.

--- SubFunctions/cutLongBoneMesh.m
+++ SubFunctions/cutLongBoneMesh.m
@@ -1,4 +1,4 @@
-function [TrPRox, TrDist] = cutLongBoneMesh(TrLB)
+function [TrProx, TrDist] = cutLongBoneMesh(TrLB)
 %CUTLONGBONEMESH Separate the Mesh of long bone io two parts, a proximal 
 % and a distal one.
 % WARNING : Assumption about the CT coordinate system :
@@ -17,19 +17,28 @@ Z0 = sign(Z0(3))*Z0;
 % Fast and dirty wat to split the bone
 LengthBone = max(TrLB.Points*Z0) - min(TrLB.Points*Z0);
 
-Zprox = max(TrLB.Points*Z0) - 0.48* LengthBone;
+Zprox = max(TrLB.Points*Z0) - 0.3* LengthBone;
 ElmtsProx = find(TrLB.incenter*Z0>Zprox);
 TrProx = TriReduceMesh( TrLB, ElmtsProx);
 TrProx = TriFillPlanarHoles( TrProx );
 
-Zdist = min(TrLB.Points*Z0) + 0.48* LengthBone;
+Zdist = min(TrLB.Points*Z0) + 0.3* LengthBone;
 ElmtsDist = find(TrLB.incenter*Z0<Zdist);
 TrDist = TriReduceMesh( TrLB, ElmtsDist);
 TrDist = TriFillPlanarHoles( TrDist );
 
 
+figure()
+% Plot the whole tibia, here ProxTib is a Matlab triangulation object
+% trisurf(TrDist,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',1,'edgecolor','none');
+% hold on
+% axis equal
 
-
+trisurf(TrDist,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',0.7,'edgecolor','none');
+hold on
+axis equal
+trisurf(TrProx,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',0.7,'edgecolor','none');
+lighting gouraud
 
 % Alt = linspace( min(TrLB.Points*Z0)+0.5 ,max(TrLB.Points*Z0)-0.5, 100);
 % Area= zeros(size(Alt));
