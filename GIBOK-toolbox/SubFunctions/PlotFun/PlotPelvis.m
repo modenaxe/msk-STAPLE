0d98f58cd5baa7702233f2b80a170e0fe52d9723
Luca Modenese <l.modenese@imperial.ac.uk>
2019-12-16 17:23:14 +0000

    (feat) plots pelvis consistently with other bones

--- GIBOK-toolbox/SubFunctions/PlotFun/PlotPelvis.m
+++ GIBOK-toolbox/SubFunctions/PlotFun/PlotPelvis.m
@@ -1,3 +1,9 @@
+%-------------------------------------------------------------------------%
+% Copyright (c) 2019 Modenese L.                                          %
+%                                                                         %
+%    Author:   Luca Modenese, April 2018                                  % 
+%    email:    l.modenese@sheffield.ac.uk                                 %
+% ----------------------------------------------------------------------- %
 function PlotPelvis( CS, TrObjects )
 %PLOTPELVIS Display figures of the femur
 %   Detailed explanation goes here
