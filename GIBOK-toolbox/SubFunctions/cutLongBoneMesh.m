5fe0c8ce6f029577a88c65ec251935266e223859
Luca Modenese <l.modenese@imperial.ac.uk>
2020-01-22 12:50:37 +0000

    add option to control bone cut ratio

--- GIBOK-toolbox/SubFunctions/cutLongBoneMesh.m
+++ GIBOK-toolbox/SubFunctions/cutLongBoneMesh.m
@@ -1,11 +1,18 @@
-function [TrProx, TrDist] = cutLongBoneMesh(TrLB)
+function [TrProx, TrDist] = cutLongBoneMesh(TrLB, varargin)
 %CUTLONGBONEMESH Separate the Mesh of long bone io two parts, a proximal 
 % and a distal one.
 % WARNING : Assumption about the CT coordinate system :
-%               Zct is approximetly distal to proximal
+%               Zct is approximately distal to proximal
+
+% to customize cuts of the entire bone
+if isempty(varargin)
+    L_ratio = 0.33;
+else
+    L_ratio = varargin{1};
+end
 
 % Get eigen vectors V_all of the Long Bone 3D geometry and volumetric center
-[ V_all, CenterVol ] = TriInertiaPpties( TrLB );
+[ V_all, ~ ] = TriInertiaPpties( TrLB );
 
 % Initial estimate of the Distal-to-Proximal (DP) axis Z0
 Z0 = V_all(:,1);
@@ -13,21 +20,22 @@ Z0 = V_all(:,1);
 % Assumption CT CS orientation
 Z0 = sign(Z0(3))*Z0;
 
-
-% Fast and dirty wat to split the bone
+% Fast and dirty way to split the bone
 LengthBone = max(TrLB.Points*Z0) - min(TrLB.Points*Z0);
 
-Zprox = max(TrLB.Points*Z0) - 0.33* LengthBone;
+% create the proximal bone part
+Zprox = max(TrLB.Points*Z0) - L_ratio* LengthBone;
 ElmtsProx = find(TrLB.incenter*Z0>Zprox);
 TrProx = TriReduceMesh( TrLB, ElmtsProx);
 TrProx = TriFillPlanarHoles( TrProx );
 
-Zdist = min(TrLB.Points*Z0) + 0.33* LengthBone;
+% create the distal bone part
+Zdist = min(TrLB.Points*Z0) + L_ratio* LengthBone;
 ElmtsDist = find(TrLB.incenter*Z0<Zdist);
 TrDist = TriReduceMesh( TrLB, ElmtsDist);
 TrDist = TriFillPlanarHoles( TrDist );
-% 
-% 
+
+
 % figure()
 % % Plot the whole tibia, here ProxTib is a Matlab triangulation object
 % % trisurf(TrDist,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',1,'edgecolor','none');
