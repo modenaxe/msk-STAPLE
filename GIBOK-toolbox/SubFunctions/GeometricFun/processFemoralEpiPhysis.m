f0b106e771ca09577a918a9d18c6b740361370d6
Luca Modenese <l.modenese@imperial.ac.uk>
2020-01-23 16:52:30 +0000

    add comments, if from main moved here

--- GIBOK-toolbox/SubFunctions/GeometricFun/processFemoralEpiPhysis.m
+++ GIBOK-toolbox/SubFunctions/GeometricFun/processFemoralEpiPhysis.m
@@ -1,6 +1,12 @@
-function [IdCdlPts, U_Axes, Orientation] = processFemoralEpiPhysis(EpiFem, CSs, Inertia_Vects)
+function [IdCdlPts, U_Axes, med_lat_ind] = processFemoralEpiPhysis(EpiFem, CSs, Inertia_Vects, edge_threshold, axes_dev_thresh)
 
-% TODO: expose proportion of max edge length
+
+
+%=========================
+% UNEXPOSED PARAMETERS
+%=========================
+% default axes_dev_thresh = 0.75;
+% default edge_threshold = 0.5;
 
 % gets largest convex hull
 [ IdxPointsPair , Edges ] = LargestEdgeConvHull(  EpiFem.Points );
@@ -12,10 +18,9 @@ i=0;
 Ikept = [];
 
 % Keep elements that are not connected to the proximal cut and that are
-% longer than half of the longest Edge
+% longer than half of the longest Edge (default)
 % NB Edges is an ordered vector -> first is largest
-% Maybe median would be less arbitrary?
-while length(Ikept) ~= sum(Edges>0.5*Edges(1))
+while length(Ikept) ~= sum(Edges>edge_threshold*Edges(1))
     i=i+1;
     if ~any(IdxPointsPair(i,1)==Idx_Epiphysis_Pts_DF_Slice) &&...
             ~any(IdxPointsPair(i,2)==Idx_Epiphysis_Pts_DF_Slice)
@@ -51,21 +56,36 @@ IdCdlPts(I_Axes_duplicate,:)=[];
 Axes(I_Axes_duplicate,:)=[];
 U_Axes = Axes./repmat(sqrt(sum(Axes.^2,2)),1,3);
 
-% Make all the axes point in the Laterat -> Medial direction
+% Are CSs.Y0 and U_Axes pointing in the same direction?
 Orientation = round(mean(sign(U_Axes*CSs.Y0)));
+% Make all the axes point in the Laterat -> Medial direction
 U_Axes = Orientation*U_Axes;
-Axes = Orientation*Axes;
+% Axes = Orientation*Axes;
 
 % delete if too far from inertial medio-Lat axis;
 % [LM] 0.75 -> acod(0.75) roughly 41 deg
-IdCdlPts(abs(U_Axes*Inertia_Vects(:,2))<0.75,:) = [];
-U_Axes(abs(U_Axes*Inertia_Vects(:,2))<0.75,:) = [];
+ind_deviant_axes = abs(U_Axes*Inertia_Vects(:,2))<axes_dev_thresh;
+IdCdlPts(ind_deviant_axes,:) = [];
+U_Axes(ind_deviant_axes,:) = [];
 
 % region growing (point, seed, radius)
 [ U_Axes_Good] = PCRegionGrowing(U_Axes, normalizeV( mean(U_Axes) )', 0.1);
 LIA = ismember(U_Axes,U_Axes_Good,'rows');
 U_Axes(~LIA,:) = [];
-Axes(~LIA,:) = [];
+% Axes(~LIA,:) = [];
 IdCdlPts(~LIA,:) = [];
+
+% Assign indices of points on Lateral or Medial Condyles Variable
+
+if Orientation < 0
+    med_lat_ind = [2 1];
+%     IdxPtsCondylesLat = IdCdlPts(:,1);
+%     IdxPtsCondylesMed = IdCdlPts(:,2);
+else
+    med_lat_ind = [1 2];
+%     IdxPtsCondylesMed = IdCdlPts(:,1);
+%     IdxPtsCondylesLat = IdCdlPts(:,2);
+end
+
 
 end 
