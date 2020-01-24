2768a8806646306f4cd91c06e4d1e02be6b4a5d1
Luca Modenese <l.modenese@imperial.ac.uk>
2020-01-24 17:04:52 +0000

    (feat) create ACs fitting ellypsoids to posterior fem condyles

---
+++ GIBOK-toolbox/SubFunctions/GeometricFun/createFemurCoordSystEllipsoidOnCondyles.m
@@ -0,0 +1,33 @@
+function CSs = createFemurCoordSystEllipsoidOnCondyles(Condyle_1,Condyle_2, CSs)
+
+% fitting ellipsoids
+center1 = ellipsoid_fit( Condyle_1.Points , '' );
+center2 = ellipsoid_fit( Condyle_2.Points , '' );
+
+% normalize and check axis direction
+Yelpsd =  normalizeV(center2-center1);
+Yelpsd = sign(Yelpsd'*CSs.Y0)*Yelpsd;
+
+% knee joint centre is midpoint of ellipsoid centres
+KneeCenterElpsd = 0.5*(center2+center1)';
+
+% define axes
+Zend =  normalizeV( CSs.CenterFH - KneeCenterElpsd);
+Xend =  normalizeV(cross(Yelpsd, Zend));
+Yend = cross(Zend, Xend);
+
+% [LM] this was in the original GIBOK code.
+% not needed, the Yend)sph is actually defined in the same way as Yend
+% here.
+% Yend = sign(Yend'*Yend_sph)*Yend;
+% Xend = cross(Yend,Zend);
+
+% store axes
+CSs.CE.Yelpsd   = Yelpsd;
+CSs.CE.Origin   = KneeCenterElpsd;
+CSs.CE.X        = Xend;
+CSs.CE.Y        = Yend;
+CSs.CE.Z        = Zend;
+CSs.CE.V        = [Xend Yend Zmech];
+
+end
