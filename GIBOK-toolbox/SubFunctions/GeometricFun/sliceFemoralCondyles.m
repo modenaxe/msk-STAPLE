a067a88bb42d0d958fbb7546f8bbb239c8cff286
Luca Modenese <l.modenese@imperial.ac.uk>
2020-01-28 16:13:39 +0000

    improve exit slicing checks

--- GIBOK-toolbox/SubFunctions/GeometricFun/sliceFemoralCondyles.m
+++ GIBOK-toolbox/SubFunctions/GeometricFun/sliceFemoralCondyles.m
@@ -1,4 +1,7 @@
 % used by KaiFemur
+% slice starts with just one curve, until it slices two condyles
+% when there are two curves, the next time there is one is the end of
+% loop
 function [Ok_FC_Pts1, Ok_FC_Pts2] = sliceFemoralCondyles(DistFem, X0)
 
 % X0 points backwards in GIBOK
@@ -7,11 +10,9 @@ function [Ok_FC_Pts1, Ok_FC_Pts2] = sliceFemoralCondyles(DistFem, X0)
 [~ , I_Post_FC] = max( DistFem.Points*-X0 );
 MostPostPoint = DistFem.Points(I_Post_FC,:);
 
-Nbr_of_curves  = 2;
 Ok_FC_Pts1 = [];
 Ok_FC_Pts2 = [];
 d = MostPostPoint*-X0 - 0.25;
-StillStart = 1;
 count = 1;
 
 %================
@@ -21,54 +22,52 @@ light; lighting phong; % light
 hold on, axis equal
 plot3(MostPostPoint(:,1), MostPostPoint(:,2), MostPostPoint(:,3),'g*', 'LineWidth', 3.0);
 %================
+
 keep_slicing = 1;
-while Nbr_of_curves == 2
-    % counting slices
-    disp(['section #',num2str(count)])
-    count = count+1;
-    
+while keep_slicing
+
     [ Curves , ~, ~ ] = TriPlanIntersect(DistFem, X0 , d );
     Nbr_of_curves = length(Curves);
-    Nbr_of_curves0 = length(Curves);
+    
+    % counting slices
+    disp(['section #',num2str(count),': ', num2str(Nbr_of_curves),' curves.'])
+    count = count+1;
     
     % plot curves
-    c_set = ['b', 'r'];
+    c_set = ['r', 'b'];
     if ~isempty(Curves)
         for c = 1:Nbr_of_curves
             plot3(Curves(c).Pts(:,1), Curves(c).Pts(:,2), Curves(c).Pts(:,3),c_set(c)); hold on; axis equal
         end
     end
 
-    %
-    Centroid = [];
-    Dist2MostPostPoint = [];
-    
-    for cn = 1:Nbr_of_curves
-        Centroid(cn,:) = mean(Curves(cn).Pts);
-        Dist2MostPostPoint(cn) = norm(MostPostPoint-Centroid(cn,:));
+    % stop if just one curve is found after there has been a second profile
+    if Nbr_of_curves == 1 && ~isempty(Ok_FC_Pts2)
+        break
+    else
+        % next slicing plane moved by 1 mm
+        d = d - 1;
     end
     
-    % slice starts with just one curve, until it slices two condyles
-    % when there are two curves, the next time there is one is the end of
-    % loop
-    if Nbr_of_curves == 1 && StillStart == 1
-        StillStart = 1;
-        Nbr_of_curves = 2;
-        
-    elseif Nbr_of_curves > 1 && StillStart == 1
-        StillStart = 0;
-    end
-    
-    if StillStart == 1
-        Ok_FC_Pts1 = [Ok_FC_Pts1;Curves.Pts];
-    elseif Nbr_of_curves0 == 2 && StillStart == 0
+    % otherwise store slices
+    if Nbr_of_curves == 1
+        Ok_FC_Pts1 = [Ok_FC_Pts1; Curves.Pts];
+    elseif Nbr_of_curves == 2
+        % needs to identify which is the section near starting point
+        % using distance from centroid for that
+        Centroid = [];
+        Dist2MostPostPoint = [];
+        for cn = 1:Nbr_of_curves
+            Centroid(cn,:) = mean(Curves(cn).Pts);
+            Dist2MostPostPoint(cn) = norm(MostPostPoint-Centroid(cn,:));
+        end
+        % closest curve to Dist2MostPostPoint is the one that started as
+        % single curve
         [~,IcurvePost1] = min(Dist2MostPostPoint);
-        Ok_FC_Pts1 = [Ok_FC_Pts1;Curves(IcurvePost1).Pts];
-        Ok_FC_Pts2 = [Ok_FC_Pts2;Curves(3-IcurvePost1).Pts];
+        % store data accordingly
+        Ok_FC_Pts1 = [Ok_FC_Pts1; Curves(IcurvePost1).Pts];
+        Ok_FC_Pts2 = [Ok_FC_Pts2; Curves(3-IcurvePost1).Pts];
     end
-    
-    % slicing plane moved by 1 mm
-    d = d - 1;
 end
 
 end
