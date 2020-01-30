2884063316dab366ecc1820177232e0fb049bb4b
modenaxe <luca.modenese@gmail.com>
2020-01-30 01:07:13 +0000

    (fix) now slice fem head in correct direction (Z0 maybe inverted?)

--- GIBOK-toolbox/SubFunctions/GeometricFun/findFemoralHead_Kai2014.m
+++ GIBOK-toolbox/SubFunctions/GeometricFun/findFemoralHead_Kai2014.m
@@ -1,19 +1,43 @@
 function [CSs, Curves] = findFemoralHead_Kai2014(ProxFem, CSs)
+
+% TODO needs sections plots
+% TODO understand axis
+% TODO: something wrong with the second while loop
+corr_dir = -1;
+disp('Computing Femoral Head Centre (Kai et al. 2014)...')
+
 % Find the most proximal point
 [~ , I_Top_FH] = max( ProxFem.Points*CSs.Z0 );
 MostProxPoint = ProxFem.Points(I_Top_FH,:);
+
+%================
+figure
+trisurf(ProxFem.ConnectivityList, ProxFem.Points(:,1), ProxFem.Points(:,2), ProxFem.Points(:,3),'Facecolor','m','Edgecolor','none');
+light; lighting phong; % light
+hold on, axis equal
+plot3(MostProxPoint(:,1), MostProxPoint(:,2), MostProxPoint(:,3),'g*', 'LineWidth', 3.0);
+%================
 
 % Get the oultine of all the proximal
 Nbr_of_curves  = 1;
 Ok_FH_Pts = [];
 d = MostProxPoint*CSs.Z0 - 0.25;
 while Nbr_of_curves == 1
-    [ Curves , ~, ~ ] = TriPlanIntersect(ProxFem, CSs.Z0 , d );
+    [ Curves , ~, ~ ] = TriPlanIntersect(ProxFem, corr_dir*CSs.Z0 , d );
     Nbr_of_curves = length(Curves);
     if Nbr_of_curves ==1
         Ok_FH_Pts = [Ok_FH_Pts;Curves.Pts];
     end
     d = d - 1;
+    
+        % plot curves
+    c_set = ['r', 'b'];
+    if ~isempty(Curves)
+        for c = 1:Nbr_of_curves
+            plot3(Curves(c).Pts(:,1), Curves(c).Pts(:,2), Curves(c).Pts(:,3),'r'); hold on; axis equal
+        end
+    end
+    
 end
 
 while Nbr_of_curves > 1
@@ -27,13 +51,28 @@ while Nbr_of_curves > 1
     [~,IcurveMed] = min(Dist2MostProxPoint);
     Ok_FH_Pts = [Ok_FH_Pts;Curves(IcurveMed).Pts];
     
-    [ Curves , ~, ~ ] = TriPlanIntersect(ProxFem, CSs.Z0 , d );
+    [ Curves , ~, ~ ] = TriPlanIntersect(ProxFem, corr_dir*CSs.Z0 , d );
     Nbr_of_curves = length(Curves);
     d = d - 1;
+    
+        % plot curves
+    c_set = 'm';%, 'g'];
+    if ~isempty(Curves)
+        for c = 1:Nbr_of_curves
+            plot3(Curves(c).Pts(:,1), Curves(c).Pts(:,2), Curves(c).Pts(:,3),'b'); hold on; axis equal
+        end
+    end
 end
 
 
 [CenterFH, Radius] = sphereFit(Ok_FH_Pts);
+
+disp('-----------------')
+disp('Final  Estimation')
+disp('-----------------')
+disp(['Centre: ', num2str(CenterFH)]);
+disp(['Radius: ', num2str(Radius)]);
+disp('-----------------')
 
 CSs.CenterFH_Kai = CenterFH ;
 CSs.RadiusFH_Kai = Radius ;
