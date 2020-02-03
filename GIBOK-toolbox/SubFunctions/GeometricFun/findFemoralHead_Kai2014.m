e3fd132b063ca006210ea456e8f8ecb810807ab6
modenaxe <luca.modenese@gmail.com>
2020-02-04 00:55:31 +0100

    (fix) femoral head determination fixed there is a working test on the medial side of the femoral head based on the aassumption that the sagittal plane is roughly correct in teh medical images, i.e. the most proximal point is always MORE MEDIAL than the throcanted and therefore the points to keep when there are two curves are even more medial.

--- GIBOK-toolbox/SubFunctions/GeometricFun/findFemoralHead_Kai2014.m
+++ GIBOK-toolbox/SubFunctions/GeometricFun/findFemoralHead_Kai2014.m
@@ -1,18 +1,16 @@
 function [CSs, Curves] = findFemoralHead_Kai2014(ProxFem, CSs)
 
-% TODO needs sections plots
-
 % TODO understand axis
 % Z0 points upwards, but plane needs to point downwards
 
-% TODO: something wrong with the second while loop
 corr_dir = -1;
 disp('Computing Femoral Head Centre (Kai et al. 2014)...')
 
 % Find the most proximal point
 [~ , I_Top_FH] = max( ProxFem.Points*CSs.Z0 );
 MostProxPoint = ProxFem.Points(I_Top_FH,:);
-
+up = CSs.Z0;
+front = cross(MostProxPoint, up);
 %================
 figure
 trisurf(ProxFem.ConnectivityList, ProxFem.Points(:,1), ProxFem.Points(:,2), ProxFem.Points(:,3),'Facecolor','m','Edgecolor','none');
@@ -22,7 +20,6 @@ plot3(MostProxPoint(:,1), MostProxPoint(:,2), MostProxPoint(:,3),'g*', 'LineWidt
 %================
 
 % Slice the femoral head starting from the top
-Nbr_of_curves  = 1;
 Ok_FH_Pts = [];
 Ok_FH_Pts_med = [];
 d = MostProxPoint*CSs.Z0 - 0.25;
@@ -57,7 +54,15 @@ while keep_slicing
         Ok_FH_Pts = [Ok_FH_Pts; Curves.Pts];
     elseif Nbr_of_curves > 1
         for i = 1:Nbr_of_curves
-            Ok_FH_Pts_med = [Ok_FH_Pts_med; Curves(i).Pts(Curves(i).Pts(:,1)<MostProxPoint(1),:)];
+            % if I assume centre of CT/MRI is always more medial than HJC
+            % then I can work with abs values
+            ind_med_point = abs(Curves(i).Pts(:,1))<abs(MostProxPoint(1));
+            % it is however a weak solution.
+            
+            % it would be more robust to check that the cross product of 
+            % dot ( cross( (x_P-x_MostProx), Z0 ) , front ) > 0
+            ind_med_point2 = (front*bsxfun(@cross,MostProxPoint', Curves(i).Pts'))>0;
+            Ok_FH_Pts_med = [Ok_FH_Pts_med; Curves(i).Pts(ind_med_point,:)];
         end
     end
 end
