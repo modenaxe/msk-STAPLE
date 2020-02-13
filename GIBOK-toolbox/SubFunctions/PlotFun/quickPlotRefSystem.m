dba03d1e56bb8158eaf4ee23a4d4409b18d1c6a7
Luca Modenese <l.modenese@imperial.ac.uk>
2020-02-13 17:30:46 +0000

    plot scripts for debugging

---
+++ GIBOK-toolbox/SubFunctions/PlotFun/quickPlotRefSystem.m
@@ -0,0 +1,5 @@
+function quickPlotRefSystem(CS)
+
+plotArrow( CS.X, 1, CS.Origin, 60, 1, 'r')
+plotArrow( CS.Y, 1, CS.Origin, 60, 1, 'g')
+plotArrow( CS.Z, 1, CS.Origin, 60, 1, 'b')
--- GIBOK-toolbox/SubFunctions/PlotFun/quickPlotTriang.m
+++ GIBOK-toolbox/SubFunctions/PlotFun/quickPlotTriang.m
@@ -5,6 +5,11 @@ function quickPlotTriang(triangObj, face_color, new_figure)
 if nargin>2 && ~isempty(new_figure)
     figure
 end
+
+if nargin==1 || (nargin==2 && isempty(face_color))
+    face_color = [0.65    0.65    0.6290];
+end
+
 trisurf(triangObj,'Facecolor', face_color, 'edgecolor','none');
 light; lighting phong; % light
 hold on, axis equal
