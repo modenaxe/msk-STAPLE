7a8bf36d569ce5f2ecd6f53fd1f477c4ae72b86e
Luca Modenese <l.modenese@imperial.ac.uk>
2020-02-14 19:24:22 +0000

    read stl only if file has an ext, otherwise mat

--- autoMSK_functions/load_mesh.m
+++ autoMSK_functions/load_mesh.m
@@ -3,9 +3,10 @@ function tri_geom = load_mesh(a_tri_mesh_file)
 
 if ischar(a_tri_mesh_file)
     % try to open it as stl file
-    tri_geom = stlRead(a_tri_mesh_file);
-    
-    if isempty(tri_geom)
+    [~,~,ext] = fileparts(a_tri_mesh_file);
+    if strcmp(ext,'.stl')
+        tri_geom = stlRead(a_tri_mesh_file);
+    else
         geom = load(a_tri_mesh_file);
         str_name = fields(geom);
         tri_geom = geom.(str_name{1});
