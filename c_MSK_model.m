005422746a05dbba735a8a870a22c95b99bb5f21
Luca Modenese <l.modenese@imperial.ac.uk>
2020-02-19 16:01:05 +0000

    process tibia

--- c_MSK_model.m
+++ c_MSK_model.m
@@ -70,47 +70,50 @@ for nb = 1:length(body_list)
     geom_set.(cur_body_name) = cur_geom;
 end
 
-%---- PELVIS -----
-% solve reference system from geometry
-[PelvisRS, PelvisBL]  = PelvisFun(geom_set.pelvis);
-addMarkersFromStruct(osimModel, 'pelvis', PelvisBL, in_mm)
-
-% compute joint params
-pelvis_location = PelvisRS.ISB.Origin*dim_fact;
-pelvis_orientation = computeZXYAngleSeq(PelvisRS.ISB.V);
-
-% ground_pelvis
-JointParams = getJointParams('ground_pelvis');
-JointParams.parent_location     = [0.0000	0.0000	0.0000];
-JointParams.parent_orientation  = [0.0000	0.0000	0.0000];
-JointParams.child_location      = pelvis_location;
-JointParams.child_orientation   = pelvis_orientation;
-
-% create the joint
-pelvis_ground_joint = createCustomJointFromStruct(osimModel, JointParams);
-osimModel.addJoint(pelvis_ground_joint);
-
-%---- FEMUR -----
-FemurCS = computeFemurISBCoordSyst_Kai2014(geom_set.femur_r);
+% %---- PELVIS -----
+% % solve reference system from geometry
+% [PelvisRS, PelvisBL]  = PelvisFun(geom_set.pelvis);
+% addMarkersFromStruct(osimModel, 'pelvis', PelvisBL, in_mm)
+% 
+% % compute joint params
+% pelvis_location = PelvisRS.ISB.Origin*dim_fact;
+% pelvis_orientation = computeZXYAngleSeq(PelvisRS.ISB.V);
+% 
+% % ground_pelvis
+% JointParams = getJointParams('ground_pelvis');
+% JointParams.parent_location     = [0.0000	0.0000	0.0000];
+% JointParams.parent_orientation  = [0.0000	0.0000	0.0000];
+% JointParams.child_location      = pelvis_location;
+% JointParams.child_orientation   = pelvis_orientation;
+% 
+% % create the joint
+% pelvis_ground_joint = createCustomJointFromStruct(osimModel, JointParams);
+% osimModel.addJoint(pelvis_ground_joint);
+% 
+% %---- FEMUR -----
+% FemurCS = computeFemurISBCoordSyst_Kai2014(geom_set.femur_r);
 % [ FemurCSs, TrObjects ] = GIBOK_femur(geom_set.femur_r);
-
-HJC_location = FemurCS.CenterFH_Kai*dim_fact;
-femur_orientation = computeZXYAngleSeq(FemurCS.V);
-
-% % hip joint
-JointParams = getJointParams('hip_r');
-JointParams.parent_location     = HJC_location;
-JointParams.parent_orientation  = pelvis_orientation;
-JointParams.child_location      = HJC_location;
-JointParams.child_orientation   = femur_orientation;
-
-% create the joint
-hip_r = createCustomJointFromStruct(osimModel, JointParams);
-osimModel.addJoint(hip_r);
+% 
+% HJC_location = FemurCS.CenterFH_Kai*dim_fact;
+% femur_orientation = computeZXYAngleSeq(FemurCS.V);
+% 
+% % % hip joint
+% JointParams = getJointParams('hip_r');
+% JointParams.parent_location     = HJC_location;
+% JointParams.parent_orientation  = pelvis_orientation;
+% JointParams.child_location      = HJC_location;
+% JointParams.child_orientation   = femur_orientation;
+% 
+% % create the joint
+% hip_r = createCustomJointFromStruct(osimModel, JointParams);
+% osimModel.addJoint(hip_r);
 
 %---- TIBIA -----
 % defines the axis for the tibia
-CS = computeTibiaISBCoordSystKai2014(geom_set.tibia_r);
+% CS = computeTibiaISBCoordSystKai2014(geom_set.tibia_r);
+
+[CSs, TrObjects] = GIBOK_tibia(geom_set.tibia_r)
+
 TTB = autoLandmarkTibia(geom_set.tibia_r, CS, 1);
 
 % knee joint
