a9379e43e8c74740d433c22bc1be7895f7ec7d5b
renaultJB <j.baptiste.renault@gmail.com>
2020-02-20 11:08:27 +0100

    Working prototype for the foot function
    Get a coordinate system for the foot and the heel, medial distal and lateral distal meta landmarks.
    The sesamoid bones of the medial meta are annoying !!!

--- GIBOK-toolbox/RFootSoleFun.m
+++ GIBOK-toolbox/RFootSoleFun.m
@@ -52,7 +52,7 @@ Z0 = cross(X0,Y0);
 
 %Visually check the Inertia Axis orientation relative to the Talus geometry
 figure()
-% Plot the whole talus, here Talus is a Matlab triangulation object
+% Plot the whole foot, here Foot is a Matlab triangulation object
 trisurf(Foot,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',.6,'edgecolor','none');
 hold on
 axis equal
@@ -105,10 +105,10 @@ Foot2 = Foot;
 [ IdxPtsPair , EdgesLength , K] = LargestEdgeConvHull(Foot2.Points);
 
 trisurf(K,x,y,z,'Facecolor','c','FaceAlpha',.2,'edgecolor','k');
-for i = 1:round(0.02*length(IdxPtsPair))
-    plot3(x(IdxPtsPair(i,:)),y(IdxPtsPair(i,:)),z(IdxPtsPair(i,:)),...
-        'k-*','linewidth',2)
-end
+% for i = 1:round(0.02*length(IdxPtsPair))
+%     plot3(x(IdxPtsPair(i,:)),y(IdxPtsPair(i,:)),z(IdxPtsPair(i,:)),...
+%         'k-*','linewidth',2)
+% end
 
 % Convert the convexHull to triangulation object
 Foot2_CH = triangulation(K,x,y,z);
@@ -135,68 +135,161 @@ Foot2_CH_Proj = triangulation(Foot2_CH.ConnectivityList, Foot2_CH_PTS_Proj);
 
 trisurf(Foot2_CH.ConnectivityList(I,:),x,y,z,'Facecolor','b','FaceAlpha',1,'edgecolor','k');
 
+%% Attribute the triangle vertices to the heel or the metatarsus
+triangleNrml = - Foot2_CH.faceNormal(I)'; % minus to point superiorly
+triangleVerticesID = Foot2_CH.ConnectivityList(I,:);
+trianglePts = Foot2_CH.Points(triangleVerticesID,:);
+[metatarsusPts, heelPt, maxEdgeLength] = TriangleClosestPointPair(trianglePts) ;
 
+% plotDot(metatarsusPts,'r',2)
+% plotDot(heelPt,'r',2)
+
+metatarsPt1 = metatarsusPts(1,:);
+metatarsPt2 = metatarsusPts(2,:);
 
 %% Get the three points of interest 
 %   1.  Select from the longest edges those which are below a plan 
 %       parallel to the one defined by the largest triangle (the start and
 %       end of those edges must be below that plan)
+
+% Height of the foot along the triangle normal
+FootISHeigth = range(Foot2_CH.Points*triangleNrml);
+
+% Keep all edges that are above 80% of the triangle max edge length
+IdxPtsPair(EdgesLength < 0.8*maxEdgeLength, :) = [];
+
+% Verify that the points are below a plan parallel to the triangle
+% offsetted 5% superiorly
+
+GoodEdges = Foot2_CH.Points(IdxPtsPair,:)*triangleNrml < ...
+    ( mean(trianglePts)*triangleNrml + 0.5 * FootISHeigth);
+
+GoodEdges = GoodEdges(1:2:end-1,:) + GoodEdges(2:2:end,:);
+
+% Verify that both edge start and end points are close enough to the
+% triangle
+IgoodEdges = find(GoodEdges == 2);
+
+GoodIdxPointPair = IdxPtsPair(IgoodEdges, :);
+
+% for i = 1:length(GoodIdxPointPair)
+%     plot3(x(GoodIdxPointPair(i,:)),y(GoodIdxPointPair(i,:)),z(GoodIdxPointPair(i,:)),...
+%         'r-s','linewidth',2)
+% end
+
+GoodPointIdx = unique( GoodIdxPointPair(:) );
+CandidatePoints = Foot2_CH.Points(GoodPointIdx,:);
+
+% Keep the metarsus points (not too close to the heel) 
+notHeelPtsID = find( sqrt(sum(bsxfun(@minus, CandidatePoints, heelPt).^2, 2))>...
+    0.5*maxEdgeLength) ;
+metaCandidatePoints = CandidatePoints(notHeelPtsID,:);
+
+
 %   2.  Cluster the points defining the largest edges in 3 clusters
+[clusterIdx, clusterCentroids] = kmeans(metaCandidatePoints, 2);
+
 %   3.  Associate the the cluster to the medial and lateral distal points
 %       of the triangles
+IdxMeta1 = knnsearch(clusterCentroids, metatarsPt1) ;
+PtsMeta1 = metaCandidatePoints(clusterIdx == IdxMeta1, :);
+pl3t(PtsMeta1,'mo')
+
+IdxMeta2 = knnsearch(clusterCentroids, metatarsPt2) ;
+PtsMeta2 = metaCandidatePoints(clusterIdx == IdxMeta2, :);
+pl3t(PtsMeta2,'go')
+
+figure(2)
+pl3t(metaCandidatePoints(clusterIdx==1,:),'b.')
+hold on
+axis equal
+pl3t(metaCandidatePoints(clusterIdx==2,:),'r.')
+
+
 %   4.  Get the the points as the furthest one from the ones from the
 %       triangles
+Idx1 = knnsearch(PtsMeta1,metatarsPt1,'K',length(PtsMeta1)) ;
+PtMeta1_Final = PtsMeta1(Idx1(end),:) ;
+
+Idx2 = knnsearch(PtsMeta2,metatarsPt2,'K',length(PtsMeta2)) ;
+PtMeta2_Final = PtsMeta2(Idx2(end),:) ;
+
+newTriangle = [PtMeta1_Final; PtMeta2_Final; heelPt];
+newTriangleNrml = cross(  (PtMeta2_Final-PtMeta1_Final), ...
+                            (PtMeta1_Final-heelPt));
+newTriangleNrml = normalizeV(newTriangleNrml);
+newTriangleNrml = sign(newTriangleNrml'*triangleNrml)*newTriangleNrml;
+
+
 %   5.  Keep the proximal vertices of the triangle as the calcaneus tip
 
 
+%   6. Identify the medial side of the foot
+Z2 = newTriangleNrml;
+Y2 = normalizeV(cross(Z2,X1));
+X2 = cross(Y2,Z2);
 
+% Project the convex hull along the previously Y2 direction
+ZX2 = [Z2,X2];
+ProjY2 = ZX2*inv(ZX2'*ZX2)*ZX2';
 
+Foot2_CH_PTS_Proj = (ProjY2*Foot2_CH.Points')';
+Foot2_CH_Proj = triangulation(Foot2_CH.ConnectivityList, Foot2_CH_PTS_Proj);
 
+% Find the largest triangle on the projected Convex Hull
+[ Foot2_CH_Proj_Ppties ] = TriMesh2DProperties( Foot2_CH_Proj );
+[~,I] = max(Foot2_CH_Proj_Ppties.Area);
 
-%% This section will certainly disappear in the terminal version
+% Medial vector
+medialDirection = Foot2_CH.faceNormal(I)';
 
-% Get neighbour facets of the largest one if the normal are not too
-% different
-n0 = Foot2_CH.faceNormal(I);
+% Reorient Y2 accordingly
+Y2 = sign(Y2'*medialDirection)*Y2;
+X2 = cross(Y2,Z2);
 
-N = neighbors(Foot2_CH);
-Nghbrs0=I;
-Nghbrs_c=I;
-for i = 1:2
-    Nghbrs_c = N(Nghbrs_c,:);
-    Nghbrs0 = unique([Nghbrs0(:) ; Nghbrs_c(:)]);
-end
+% Determine which of the metatarsus points is medial and which is lateral.
+PtsMeta1and2_Final = [PtMeta1_Final;PtMeta2_Final];
+[~, ILat] = min(PtsMeta1and2_Final*Y2);
+[~, IMed] = max(PtsMeta1and2_Final*Y2);
 
-Nghbrs_OK = [];
-for f = Nghbrs0   
-    if n0*Foot2_CH.faceNormal(f)' > 0.9994
-        Nghbrs_OK(end+1) = f
-    end
-end  
+PtMetaLat = PtsMeta1and2_Final(ILat,:) ;
+PtMetaMed = PtsMeta1and2_Final(IMed,:) ;
 
-% Project all the facet in the plane associated to the largest facet
+%   7. Final coordinate system and bony landmarks
+CSs.X = X2; % Distal proximal
+CSs.Y = Y2; % Lateral to medial
+CSs.Z = Z2; % Ventral to dorsal
 
-% Compute the 2D convhull of the projected facet found the two longest side
-% The 3 or 4 points defined by the two longest side are the one ore two pts
-% of the calcaneus et the 1st and 5th metatarse points
+CSs.MedDistalMeta = PtMetaMed;
+CSs.LatDistalMeta = PtMetaLat;
+CSs.HeelTip = heelPt;
 
+figure(3)
+% Plot the whole foot, here Foot is a Matlab triangulation object
+trisurf(Foot,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',1,'edgecolor','none');
+hold on
+axis equal
 
-%% Try to find the direction of the meta
+% handle lighting of objects
+light('Position',CenterVol' + 500*Y0' + 500*X0','Style','local')
+light('Position',CenterVol' + 500*Y0' - 500*X0','Style','local')
+light('Position',CenterVol' - 500*Y0' + 500*X0' - 500*Z0','Style','local')
+light('Position',CenterVol' - 500*Y0' - 500*X0' + 500*Z0','Style','local')
+lighting gouraud
 
-for i =1:5
-    Foot_Start = min(Foot.Points*X0);
-    Foot_End = max(Foot.Points*X0);
-    Foot_Length = Foot_End - Foot_Start;
-    
-    % Keep only Meta -> Points located between 50% and 66% of the foot length
-    ElmtsMeta = find(Foot.incenter*X0 > (Foot_Start+0.50*Foot_Length) &...
-        Foot.incenter*X0 < (Foot_Start+0.66*Foot_Length));
-    Meta = TriReduceMesh( Foot, ElmtsMeta );
-    
-    % Get least square plane normal vector of the foot
-    [a,Z0] = lsplane(Foot.Points);
-    Y0 = normalizeV(cross(Z0,X0));
-    X0 = cross(Y0,Z0);
-end
+% Remove grid
+grid off
 
+%Plot the inertia Axis & Volumic center
+plotDot(heelPt,'k',3)
+plotDot(PtMetaLat,'b',3)
+plotDot(PtMetaMed,'r',3)
 
+% plot vectors
+plotArrow( X2, 1, heelPt, 100, 1, 'r')
+plotArrow( Y2, 1, heelPt, 50, 1, 'g')
+plotArrow( Z2, 1, heelPt, FootISHeigth, 1, 'b')
+
+% Plot the sole plane
+[x, y, z] = deal(newTriangle(:,1), newTriangle(:,2), newTriangle(:,3));
+trisurf([1 2 3],x,y,z,'Facecolor','b','FaceAlpha',0.4,'edgecolor','k');
