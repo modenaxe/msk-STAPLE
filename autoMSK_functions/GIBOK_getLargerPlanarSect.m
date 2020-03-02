d8b16ed3785875041f0a897dcc61c891ae8c22e7
modenaxe <luca.modenese@gmail.com>
2020-03-02 13:14:11 +0000

    adappt to updated code for Curves

--- autoMSK_functions/GIBOK_getLargerPlanarSect.m
+++ autoMSK_functions/GIBOK_getLargerPlanarSect.m
@@ -1,10 +1,10 @@
-function [Curve, N_curves] = GIBOK_getLargerPlanarSect(Curves)
+function [Curve, N_curves, Areas] = GIBOK_getLargerPlanarSect(Curves)
 
 N_curves = length(Curves);
 
 % check to use just the tibial curve, as in GIBOK
 for nc = 1: N_curves
-    [~, Areas(nc)] = PlanPolygonCentroid3D( Curves(1).Pts);
+    Areas(nc) = Curves(nc).Area;
 end
 [~, ind_max_area] = max(Areas);
 Curve = Curves(ind_max_area);
