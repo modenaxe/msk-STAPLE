f8e10c0f1ec04ee48ecc1cfff7e7f981f23ecb19
Luca Modenese <l.modenese@imperial.ac.uk>
2020-03-05 20:44:56 +0000

    (impr) deal with joint param defined only on parent or child

--- autoMSK_functions/getJointParams.m
+++ autoMSK_functions/getJointParams.m
@@ -1,30 +1,50 @@
 function JointParamsStruct = getJointParams(joint_name, parentBodyCS, childBodyCS)
+% the assumption is that the reference system will be provided for each
+% body. Each body will have specified the parameters for the joint.
 
+% these cases implement fixes if they do not.
+% TODO: ensure a body is connected to ground if no specifics are provided.
 if nargin>1
-    % the assumption is that the reference system will be provided for each
-    % body. Each body will have specified the parameters for the joint.
-    % TODO: ensure a body is connected to ground if no specifics are provided.
+    
+    % empty input as parentBodyCS means all set to zero
     if isempty(parentBodyCS)
         parentBodyCS.(joint_name).parent_location     = [0.0000	0.0000	0.0000];
         parentBodyCS.(joint_name).parent_orientation  = [0.0000	0.0000	0.0000];
     end
-    
-    % dealing with the cases where the joint centre is computed just in one
-    % body and set for the other
+    % if joint field is unavailable use the available one
+     if ~isfield(childBodyCS, joint_name)
+         childBodyCS.(joint_name) = [];
+     end
+     if ~isfield(parentBodyCS, joint_name)
+         parentBodyCS.(joint_name) = [];
+     end
+    % if joint location is unavailable in child use parent
     if isfield(childBodyCS.(joint_name), 'child_location')
-        JointParamsStruct.child_location      = childBodyCS.(joint_name).child_location;
+        JointParamsStruct.child_location   = childBodyCS.(joint_name).child_location;
     else
         JointParamsStruct.child_location   = parentBodyCS.(joint_name).parent_location;
     end
-    
+    % if joint location is unavailable in parent use child
     if isfield(parentBodyCS.(joint_name), 'parent_location')
-        JointParamsStruct.parent_location      = parentBodyCS.(joint_name).parent_location;
+        JointParamsStruct.parent_location   = parentBodyCS.(joint_name).parent_location;
     else
         JointParamsStruct.parent_location   = childBodyCS.(joint_name).child_location;
     end
-    % assign orientations
-    JointParamsStruct.parent_orientation     = parentBodyCS.(joint_name).parent_orientation;
-    JointParamsStruct.child_orientation      = childBodyCS.(joint_name).child_orientation;
+    % if joint orientation is unavailable in child use parent
+    if isfield(childBodyCS.(joint_name), 'child_orientation')
+        JointParamsStruct.child_orientation   = childBodyCS.(joint_name).child_orientation;
+    else
+        JointParamsStruct.child_orientation   = parentBodyCS.(joint_name).parent_orientation;
+    end
+    % if joint orientation is unavailable in parent use child
+    if isfield(parentBodyCS.(joint_name), 'parent_orientation')
+        JointParamsStruct.parent_orientation  = parentBodyCS.(joint_name).parent_orientation;
+    else
+        JointParamsStruct.parent_orientation  = childBodyCS.(joint_name).child_orientation;
+    end
+%     % assign orientations
+%     JointParamsStruct.parent_orientation     = parentBodyCS.(joint_name).parent_orientation;
+%     JointParamsStruct.child_orientation      = childBodyCS.(joint_name).child_orientation;
 end
 
 switch joint_name
