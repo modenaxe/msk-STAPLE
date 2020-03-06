14e59caeb32c4c9458a73a7bec2c9812fbdfde7f
modenaxe <luca.modenese@gmail.com>
2020-03-06 10:36:00 +0000

    add determination of med-lat direction

--- autoMSK_functions/MSK_tibia_Kai2014.m
+++ autoMSK_functions/MSK_tibia_Kai2014.m
@@ -19,23 +19,12 @@ else
     Tibia = TriUnite(DistTib, ProxTib);
     [ V_all, CenterVol] = TriInertiaPpties( Tibia );
 end
+
 warning('PCA WITH/WITHOT TIBIA NEEDS TESTNG')
+
 % checks on vertical direction
 Z0 = V_all(:,1);
 Z0 = sign((mean(ProxTib.Points)-mean(DistTib.Points))*Z0)*Z0;
-
-% need to finish
-%==================================================
-warning('need to compute ankle joint centre to ensure med-lat axis');
-% assuming tibia is in the geometry
-% Find the most distal point
-[~ , I_dist_fib] = min( Tibia.Points* -Z0 );
-MostDistalPoint = Tibia.Points(I_dist_fib,:);
-% this will point laterally, as Z should do for right side
-% med2lat = MostDistalPoint-CenterVol';
-% Y0 = med2lat';
-Y0 = V_all(:,2);
-%==================================================
 
 % Slices 1 mm apart as in Kai et al. 2014
 slices_thick = 1;
@@ -45,28 +34,27 @@ slices_thick = 1;
 [ Curves , ~, ~ ] = TriPlanIntersect(Tibia, Z0 , -AltAtMax );
 
 % keep just the largest outline (tibia section)
-N_Curves = length(Curves);
-max_area = 0;
-if N_Curves>1
-    for nc = 1:N_Curves
-        cur_area = Curves(nc).Area;
-        if cur_area>max_area
-            slice_to_fit = Curves(nc);
-        end
-    end
-else
-    slice_to_fit = Curves;
+[maxAreaSection, N_curves] = GIBOK_getLargerPlanarSect(Curves);
+
+% report if there are more than two curves
+just_tibia = 1;
+if N_curves == 2
+    disp('Tibia and fibula have been detected.')
+    just_tibia = 0;
+elseif N_curves>2
+    warning(['There are ', num2str(N_curves), ' section areas.']);
+    error('This should not be the case (only tibia and fibula should be there.')
 end
 
 % debug plots
 quickPlotTriang(Tibia,'m', 1); hold on
-for nn = 1:N_Curves
-    plot3(slice_to_fit.Pts(:,1), slice_to_fit.Pts(:,2), slice_to_fit.Pts(:,3),'r-', 'LineWidth',4); hold on
+for nn = 1:N_curves
+    plot3(maxAreaSection.Pts(:,1), maxAreaSection.Pts(:,2), maxAreaSection.Pts(:,3),'r-', 'LineWidth',4); hold on
 end
     
 % Move the outline curve points in the inertial ref system, so the vertical
 % component (:,1) is on a plane
-PtsCurves = vertcat(Curves(:).Pts)*V_all;
+PtsCurves = vertcat(maxAreaSection.Pts)*V_all;
 
 % Fit a planar ellipse to the outline of the tibia section
 FittedEllipse = fit_ellipse(PtsCurves(:,2), PtsCurves(:,3));
@@ -79,9 +67,40 @@ CenterEllipse = transpose(V_all*[mean(PtsCurves(:,1)); % constant anyway
 YElpsMax = V_all*[  0 ;
                    cos(FittedEllipse.phi);
                    -sin(FittedEllipse.phi)];
-               
+
+% compute lateral direction
+warning('==========================')
+warning('THIS NEEDS PROPER TESTING')
+warning('==========================')
+[ ~, CenterVolTibDist] = TriInertiaPpties(DistTib);
+if just_tibia
+    % Find the most distal point, it will be medial
+    [~ , I_dist_fib] = min( Tibia.Points* -Z0 );
+    MostDistalMedialPt = Tibia.Points(I_dist_fib,:);
+    U_tmp = MostDistalMedialPt - CenterVolTibDist;
+else %tibia and fibula
+    warning('==========================')
+    warning('THIS NEEDS PROPER TESTING')
+    warning('==========================')
+    % slice at the centroid of tibial distal
+    [ Curves , ~, ~ ] = TriPlanIntersect(DistTib, Z0 , -AltAtMax );
+    visualize
+    
+    % throw error if more than 2 sections
+    if length(Curves)>2; error('There are more than two sections in tibia triangulation. Please check.'); end;
+    % define the vector
+    if Curves(1).Area>Curves(2).Area
+        % vector from tibia section to fibular section
+        U_tmp = mean(Curves(2).Pts) - mean(Curves(1).Pts);
+    else
+        U_tmp = -1 * (mean(Curves(2).Pts) - mean(Curves(1).Pts));
+    end
+end
+% points medially
+Y0_temp = normalizeV(U_tmp' - (U_tmp*Z0)*Z0); 
+
 % here the assumption is that Y0 has correct m-l orientation               
-YElpsMax = sign(Y0'*YElpsMax)*YElpsMax;
+YElpsMax = sign(Y0_temp'*YElpsMax)*YElpsMax;
 
 EllipsePts = transpose(V_all*[ones(length(FittedEllipse.data),1)*PtsCurves(1) FittedEllipse.data']');
 
@@ -101,12 +120,13 @@ X = cross(Y, Z);
 Ydp_knee  = cross(Z, X);
 CS.V_knee = [X Ydp_knee Z];
 CS.knee_r.child_orientation = computeZXYAngleSeq(CS.V_knee);
+
 % the knee axis is defined by the femoral fitting
 % CS.knee_r.child_location = KneeCenter*dim_fact;
 
 % the talocrural joint is also defined by the talus fitting.
 % apart from the reference system -> NB: Z axis to switch with talus Z
-CS.ankle_r.parent_orientation = computeZXYAngleSeq(CS.V_knee);
+% CS.ankle_r.parent_orientation = computeZXYAngleSeq(CS.V_knee);
 
 % quickPlotRefSystem(CS)
 
