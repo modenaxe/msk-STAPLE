9e2f87ac93d5469048bc938955aad62d6b25e01d
modenaxe <luca.modenese@gmail.com>
2020-03-08 22:33:17 +0000

    fe(feat) plot ellipsoids

---
+++ GIBOK-toolbox/SubFunctions/PlotFun/PlotEllipsoid.m
@@ -0,0 +1,20 @@
+% Author: Luca Modenese
+% based on https://www.mathworks.com/matlabcentral/answers/19716-what-is-the-function-to-plot-a-rotating-3-d-ellipsoid
+function PlotEllipsoid(centre, radii, evecs, color, alpha)
+
+M = centre;
+% generate ellipsoid
+[xc,yc,zc] = ellipsoid(0,0,0,radii(1),radii(2),radii(3),50);
+
+U = evecs;
+
+% rotate data with orientation matrix U and center M
+a = kron(U(:,1),xc); b = kron(U(:,2),yc); c = kron(U(:,3),zc);
+data = a+b+c; n = size(data,2);
+x = data(1:n,:)+M(1); y = data(n+1:2*n,:)+M(2); z = data(2*n+1:end,:)+M(3);
+
+% [rot_x, rot_y, rot_z] = [x, y, z] * RotEllips2Ground;
+
+surf(x, y, z, 'Facecolor', color, 'FaceAlpha',alpha);
+
+end
