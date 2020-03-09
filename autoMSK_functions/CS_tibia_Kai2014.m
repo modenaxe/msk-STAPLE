a38811151c5b9fcf5db591b0674b6c9d2c25c838
Luca Modenese <l.modenese@imperial.ac.uk>
2020-03-09 19:20:03 +0000

    add visualisation and test for med-lat (FIXED GIBOK BUG)

--- autoMSK_functions/CS_tibia_Kai2014.m
+++ autoMSK_functions/CS_tibia_Kai2014.m
@@ -1,9 +1,8 @@
 
-function CS = MSK_tibia_Kai2014(Tibia, DistTib, in_mm)
+function CS = CS_tibia_Kai2014(Tibia, DistTib, debug_plot)
 
-% check units
-if nargin<3;     in_mm = 1;  end
-if in_mm == 1;     dim_fact = 0.001;  else;  dim_fact = 1; end
+% check plots
+if nargin<3;     debug_plot = 1;  end
 
 % if this is an entire tibia then cut it in two parts
 % but keep track of all geometries
@@ -20,8 +19,6 @@ else
     [ V_all, CenterVol] = TriInertiaPpties( Tibia );
 end
 
-warning('PCA WITH/WITHOT TIBIA NEEDS TESTNG')
-
 % checks on vertical direction
 Z0 = V_all(:,1);
 Z0 = sign((mean(ProxTib.Points)-mean(DistTib.Points))*Z0)*Z0;
@@ -36,22 +33,20 @@ slices_thick = 1;
 % keep just the largest outline (tibia section)
 [maxAreaSection, N_curves] = GIBOK_getLargerPlanarSect(Curves);
 
-% report if there are more than two curves
-just_tibia = 1;
-if N_curves == 2
-    disp('Tibia and fibula have been detected.')
-    just_tibia = 0;
-elseif N_curves>2
+% check number of curves
+if N_curves>2
     warning(['There are ', num2str(N_curves), ' section areas.']);
-    error('This should not be the case (only tibia and fibula should be there.')
+    error('This should not be the case (only tibia and possibly fibula should be there).')
 end
 
-% debug plots
-quickPlotTriang(Tibia,'m', 1); hold on
-for nn = 1:N_curves
-    plot3(maxAreaSection.Pts(:,1), maxAreaSection.Pts(:,2), maxAreaSection.Pts(:,3),'r-', 'LineWidth',4); hold on
+% plot max section to check
+if debug_plot
+    quickPlotTriang(Tibia,[], 1); hold on
+    for nn = 1:N_curves
+        plot3(maxAreaSection.Pts(:,1), maxAreaSection.Pts(:,2), maxAreaSection.Pts(:,3),'r-', 'LineWidth',3); hold on
+    end
 end
-    
+
 % Move the outline curve points in the inertial ref system, so the vertical
 % component (:,1) is on a plane
 PtsCurves = vertcat(maxAreaSection.Pts)*V_all;
@@ -64,65 +59,71 @@ CenterEllipse = transpose(V_all*[mean(PtsCurves(:,1)); % constant anyway
                                  FittedEllipse.X0_in;
                                  FittedEllipse.Y0_in]);
 
+% project the ellipse back to reference system
 YElpsMax = V_all*[  0 ;
                    cos(FittedEllipse.phi);
-                   -sin(FittedEllipse.phi)];
+                   sin(FittedEllipse.phi)]; % [LM: GIBOK had a -sin() (?) probable BUG]
 
-% compute lateral direction
-warning('==========================')
-warning('THIS NEEDS PROPER TESTING')
-warning('==========================')
-[ ~, CenterVolTibDist] = TriInertiaPpties(DistTib);
+% slice at centroid of distal tibia
+[ ~, CenterVolTibDist] = TriInertiaPpties( DistTib );
+d = CenterVolTibDist'*Z0;
+[ DistCurves , ~, ~ ] = TriPlanIntersect(DistTib, Z0 , -d );
+% check the number of curves on that slice
+N_DistCurves = length(DistCurves);
+just_tibia = 1;
+if N_DistCurves == 2
+    disp('Tibia and fibula have been detected.')
+    just_tibia = 0;
+elseif N_DistCurves>2
+    warning(['There are ', num2str(N_DistCurves), ' section areas.']);
+    error('This should not be the case (only tibia and possibly fibula should be there.')
+end
+
+% compute a vector pointing laterally (Z_ISB)
 if just_tibia
     % Find the most distal point, it will be medial
     [~ , I_dist_fib] = min( Tibia.Points* -Z0 );
     MostDistalMedialPt = Tibia.Points(I_dist_fib,:);
     % vector pointing laterally
     U_tmp = CenterVolTibDist'- MostDistalMedialPt;
-else %tibia and fibula
-    warning('==========================')
-    warning('THIS NEEDS PROPER TESTING')
-    warning('==========================')
-    % slice at the centroid of tibial distal
-    d = CenterVolTibDist*Z0;
-    [ Curves , ~, ~ ] = TriPlanIntersect(DistTib, Z0 , -d );
-    quickPlotTriang(DistTib, 'g', 1); hold on
-    plot3(Curves.Pts(:,1), Curves.Pts(:,2), Curves.Pts(:,3),'r-', 'LineWidth',4); hold on
-    
-    % throw error if more than 2 sections
-    if length(Curves)>2; error('There are more than two sections in tibia triangulation. Please check.'); end
-    % define the vector
-    if Curves(1).Area>Curves(2).Area
+else
+    %tibia and fibula
+    % check which area is larger(Tibia)
+    if DistCurves(1).Area>DistCurves(2).Area
         % vector from tibia section to fibular section
-        U_tmp = mean(Curves(2).Pts) - mean(Curves(1).Pts);
+        U_tmp = mean(DistCurves(2).Pts) - mean(DistCurves(1).Pts);
     else
-        U_tmp = -1 * (mean(Curves(2).Pts) - mean(Curves(1).Pts));
+        U_tmp = -1 * (mean(DistCurves(2).Pts) - mean(DistCurves(1).Pts));
     end
 end
-% points medially
+
+% making U_temp normal to Z0 (still points laterally)
 Y0_temp = normalizeV(U_tmp' - (U_tmp*Z0)*Z0); 
 
 % here the assumption is that Y0 has correct m-l orientation               
 YElpsMax = sign(Y0_temp'*YElpsMax)*YElpsMax;
 
-EllipsePts = transpose(V_all*[ones(length(FittedEllipse.data),1)*PtsCurves(1) FittedEllipse.data']');
-
-% Store body info
-CS.Origin        = CenterEllipse*dim_fact;
-CS.CenterVol     = CenterVol;
-% CS.CenterKnee  = CenterEllipse;
-CS.ElpsMaxPtVect = YElpsMax;
-CS.ElpsPts       = EllipsePts;
+% EllipsePts = transpose(V_all*[ones(length(FittedEllipse.data),1)*PtsCurves(1) FittedEllipse.data']');
 
 % common axes: X is orthog to Y and Z, which are not mutually perpend
 Y = normalizeV(Z0);
 Z = normalizeV(YElpsMax);
 X = cross(Y, Z);
 
+% segment reference system
+CS.Origin        = CenterVol;
+% CS.CenterKnee  = CenterEllipse;
+% CS.ElpsMaxPtVect = YElpsMax;
+% CS.ElpsPts       = EllipsePts;
+CS.X = X;
+CS.Y = Y;
+CS.Z = Z;
+
 % define the knee reference system
 Ydp_knee  = cross(Z, X);
-CS.V_knee = [X Ydp_knee Z];
-CS.knee_r.child_orientation = computeZXYAngleSeq(CS.V_knee);
+JCS.knee_r.Origin = CenterEllipse;
+JCS.knee_r.V = [X Ydp_knee Z];
+JCS.knee_r.child_orientation = computeZXYAngleSeq(JCS.knee_r.V);
 
 % the knee axis is defined by the femoral fitting
 % CS.knee_r.child_location = KneeCenter*dim_fact;
@@ -131,6 +132,9 @@ CS.knee_r.child_orientation = computeZXYAngleSeq(CS.V_knee);
 % apart from the reference system -> NB: Z axis to switch with talus Z
 % CS.ankle_r.parent_orientation = computeZXYAngleSeq(CS.V_knee);
 
-% quickPlotRefSystem(CS)
+if debug_plot
+    quickPlotRefSystem(CS);
+    quickPlotRefSystem(JCS.knee_r)
+end
 
 end
