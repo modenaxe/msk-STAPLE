17eac09eded4cfaefccda5ea34cc34f9743b23bd
Luca Modenese <l.modenese@imperial.ac.uk>
2020-03-11 20:24:39 +0000

    (feat) plots nicely triangulations. Makes bone specific funcs redundant

---
+++ GIBOK-toolbox/SubFunctions/PlotFun/PlotTriangLight.m
@@ -0,0 +1,37 @@
+function PlotTriangLight(Triang, CS, new_plot)
+
+if nargin<3
+    figure()
+elseif new_plot==1
+    figure()
+end
+
+if nargin<2
+    CS = [];
+end
+
+% Plot the whole tibia, here TrObjects.Femur is a Matlab triangulation object
+trisurf(Triang,'Facecolor',[0.65    0.65    0.6290],'FaceAlpha',1,'edgecolor','none');
+hold on
+axis equal
+
+if ~isempty(CS)
+    
+    if ~isfield(CS,'Y') && isfield(CS,'V')
+        CS.X = CS.V(:,1);
+        CS.Y = CS.V(:,2);
+        CS.Z = CS.V(:,3);
+    end
+    
+    % handle lighting of objects
+    light('Position',CS.Origin + 500*CS.Y + 500*CS.X,'Style','local')
+    light('Position',CS.Origin + 500*CS.Y - 500*CS.X,'Style','local')
+    light('Position',CS.Origin - 500*CS.Y + 500*CS.X - 500*CS.Z,'Style','local')
+    light('Position',CS.Origin - 500*CS.Y - 500*CS.X + 500*CS.Z,'Style','local')
+    lighting gouraud
+end
+
+% Remove grid
+grid off
+
+end
