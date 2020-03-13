7227eb7750f254044234b071c77d76738df2dc2b
modenaxe <luca.modenese@gmail.com>
2020-03-13 16:30:24 +0000

    funcunc to create all joints given the JCS structure. The idea is to use JCS has unique collector of joint parameters to maximise encapsulation

---
+++ autoMSK_functions/createLowerLimbJoints.m
@@ -0,0 +1,37 @@
+function createLowerLimbJoints(osimModel, JCS)
+
+% ground_pelvis joint
+JointParams = getJointParams('ground_pelvis', [], JCS.pelvis);
+pelvis_ground_joint = createCustomJointFromStruct(osimModel, JointParams);
+osimModel.addJoint(pelvis_ground_joint);
+
+% hip joint
+JointParams = getJointParams('hip_r', JCS.pelvis, JCS.femur_r);
+hip_r = createCustomJointFromStruct(osimModel, JointParams);
+osimModel.addJoint(hip_r);
+
+% knee joint
+JointParams = getJointParams('knee_r', JCS.femur_r, JCS.tibia_r);
+knee_r = createCustomJointFromStruct(osimModel, JointParams);
+osimModel.addJoint(knee_r);
+
+% ankle joint
+JCS.tibia_r = assembleAnkleParentOrientation(JCS.tibia_r, JCS.talus_r);
+JointParams = getJointParams('ankle_r', JCS.tibia_r, JCS.talus_r);
+ankle_r = createCustomJointFromStruct(osimModel, JointParams);
+osimModel.addJoint(ankle_r);
+
+% subtalar joint
+JointParams = getJointParams('subtalar_r', JCS.talus_r, JCS.calcn_r);
+subtalar_r = createCustomJointFromStruct(osimModel, JointParams);
+osimModel.addJoint(subtalar_r);
+
+% patella joint
+if isfield(JCS, 'patella_r')
+    JCS.patella_r = assemblePatellofemoralParentOrientation(JCS.femur_r, JCS.patella_r);
+    JointParams = getJointParams('patellofemoral_r', JCS.femur_r, JCS.patella_r);
+    patfem_r = createCustomJointFromStruct(osimModel, JointParams);
+    osimModel.addJoint(patfem_r);
+end
+
+end
