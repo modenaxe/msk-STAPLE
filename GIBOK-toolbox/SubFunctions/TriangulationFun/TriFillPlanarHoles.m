d6da567075814dd64eae8c805ce5ca4bc6cb19d9
renaultJB <j.baptiste.renault@gmail.com>
2020-03-19 15:00:00 +0100

    Fix TriPlanarFillHoles for multiple Holes

--- GIBOK-toolbox/SubFunctions/TriangulationFun/TriFillPlanarHoles.m
+++ GIBOK-toolbox/SubFunctions/TriangulationFun/TriFillPlanarHoles.m
@@ -5,30 +5,97 @@ function [ TRout ] = TriFillPlanarHoles( TRin)
 
 FB = TRin.freeBoundary;
 
-HoleCenter = mean(TRin.Points(FB(:,1),:));
-TriCenter =  mean(TRin.Points);
+Pts = TRin.Points;
+Segments = FB;
 
-NewNode = length(TRin.Points)+1;
-NewNodes = [TRin.Points;HoleCenter];
-
-NewElements = [FB,ones(length(FB),1)*NewNode];
-
-%% Check that the normals of the newly created element are properlu oriented
-U = HoleCenter-TriCenter; U = U'/norm(U);
-Vctrs1 = NewNodes(NewElements(:,2),:)' - NewNodes(NewElements(:,1),:)';
-Vctrs2 = NewNodes(NewElements(:,3),:)' - NewNodes(NewElements(:,1),:)';
-
-normals = transpose(cross(Vctrs1,Vctrs2));
-normals = normals./repmat(sqrt(sum(normals.^2,2)),1,3);
-
-% Invert node ordering if the normals are inverted
-if mean(normals*U)<0
-    NewElements = [FB(:,1),ones(length(FB),1)*NewNode,FB(:,2)];
+%%  Found and separate the holes
+j=1;
+Curves=struct();
+i=1;
+while ~isempty(Segments)
+    % Initialise the Curves Structure, if there are multiple curves this
+    % will lead to trailing zeros that will be removed afterwards
+    Curves(i).NodesID = zeros(length(Pts),1);
+    Curves(i).FB = [];
+    
+    % Initialise the first segment
+    Curves(i).NodesID(j)=Segments(1,1);
+    Curves(i).NodesID(j+1)=Segments(1,2);
+    Curves(i).FB(end+1,:) = Segments(1,:);
+    
+    % Remove the semgents added to Curves(i) from the segments matrix
+    Segments(1,:)=[];
+    j=j+1;
+    
+    % Find the edge in segments that has a node already in the curves(i).NodesID
+    % This edge will be the next edge of the current curve because it's
+    % connected to the current segment
+    % Is, the index of the next edge
+    % Js, the index of the node within this edge already present in NodesID
+    [Is,Js] = ind2sub(size(Segments),find(Segments(:) == Curves(i).NodesID(j)));
+    
+    % Nk is the node of the previuously found edge that is not in the
+    % current curves(i).NodeID list
+    % round(Js+2*(1.5-Js)) give 1 if Js = 2 and 2 if Js = 1
+    % It gives the other node not yet in NodesID of the identified next edge
+    Nk = Segments(Is,round(Js+2*(1.5-Js)));
+    
+    Curves(i).FB(end+1,:) = Segments(Is,:);
+    Segments(Is,:)=[];
+    j=j+1;
+    % Loop until there is no next node
+    while ~isempty(Nk)
+        Curves(i).NodesID(j) = Nk;
+        [Is,Js] = ind2sub(size(Segments),find(Segments(:) == Curves(i).NodesID(j)));
+        Nk = Segments(Is,round(Js+2*(1.5-Js)));
+        if ~isempty(Nk)
+            Curves(i).FB(end+1,:) = Segments(Is,:);
+        end
+        Segments(Is,:)=[];
+        j=j+1;
+    end
+    % If there is on next node then we move to the next curve
+    i=i+1;
 end
 
-%% Write output results
-NewConnectivityList = [TRin.ConnectivityList;NewElements];
+for i = 1 : length(Curves)
+    Curves(i).NodesID(Curves(i).NodesID==0) = [];
+    Curves(i).Pts = Pts(Curves(i).NodesID,:);
+end
+
+
+%% Fill the holes
+NewNodes = TRin.Points;
+NewNode = length(TRin.Points);
+TriCenter =  mean(TRin.Points);
+NewConnectivityList = TRin.ConnectivityList;
+
+for i = 1 : length(Curves)
+    HoleCenter = mean(Curves(i).Pts);
+    NewNode = NewNode + 1;
+    NewNodes(end+1,:) = HoleCenter;
+    
+    NewElements = [Curves(i).FB,ones(length(Curves(i).FB),1)*NewNode];
+    
+    U = HoleCenter-TriCenter; U = U'/norm(U);
+    Vctrs1 = NewNodes(NewElements(:,2),:)' - NewNodes(NewElements(:,1),:)';
+    Vctrs2 = NewNodes(NewElements(:,3),:)' - NewNodes(NewElements(:,1),:)';
+    
+    normals = transpose(cross(Vctrs1,Vctrs2));
+    normals = normals./repmat(sqrt(sum(normals.^2,2)),1,3);
+    
+    % Invert node ordering if the normals are inverted
+    if mean(normals*U)<0
+        NewElements = [Curves(i).FB(:,1),...
+            ones(length(Curves(i).FB),1)*NewNode,...
+            Curves(i).FB(:,2)];
+    end
+    
+    NewConnectivityList = cat(1, NewConnectivityList, NewElements);
+end
+
 TRout = triangulation(NewConnectivityList,NewNodes);
+
 
 end
 
