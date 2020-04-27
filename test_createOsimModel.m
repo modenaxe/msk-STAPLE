40e6c946bbc7e78b6a5c7ff8386d8a118e2eb6f4
Luca Modenese <l.modenese@imperial.ac.uk>
2020-04-27 12:36:59 +0100

    existence check moved in load mesh func

--- test_createOsimModel.m
+++ test_createOsimModel.m
@@ -9,21 +9,22 @@ addpath(genpath('FemPatTibACS/KneeACS/Tools'));
 dataset_set = {'LHDL_CT', 'P0_MRI', 'JIA_CSm6', 'TLEM2_CT'};
 body_list = {'pelvis_no_sacrum','femur_r','tibia_r','talus_r', 'calcn_r'};
 in_mm = 1;
-n_d = 3;
+n_d = 1;
 %--------------------------------------
 
 % setup folders
 model_name = dataset_set{n_d};
 main_ds_folder =  ['test_geometries',filesep,dataset_set{n_d}];
-tri_folder = fullfile(main_ds_folder,'tri');
+tri_folder = fullfile(main_ds_folder,'stl');
 vis_geom_folder=fullfile(main_ds_folder,'vtp');
 
+tic
 for nb = 1:numel(body_list)
     cur_body_name = body_list{nb};
     cur_geom_file = fullfile(tri_folder, cur_body_name);
-    if exist([cur_geom_file,'.mat'],'file')~=2; disp([cur_body_name,' geometry not available.']);continue; end
     geom_set.(cur_body_name) = load_mesh(cur_geom_file);
 end
+disp(['Geometries imported in ', num2str(toc), ' s']);
 
 % create bodies
 osimModel = createBodiesFromBoneGeometries(geom_set, vis_geom_folder);
