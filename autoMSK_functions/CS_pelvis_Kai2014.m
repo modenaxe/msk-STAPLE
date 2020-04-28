372a70c781aa4599b1c3c107d48126fb0fe48ad5
Luca Modenese <l.modenese@imperial.ac.uk>
2020-04-28 19:21:40 +0100

    (feat) implement Kai2014 Pelvis processing. See comments->Results are the same as GIBOK_pelvis because both methods rely on the principal inertial axes to define directions and therefore the bony landmarks used to define the ISB ref system will result the same. GIBOK however has an automated initialization to identify the direction of the axes, and as such is preferable. NOTE: the pubic symphisis are the only point that changes and GIBOK_pelvis does a better job in identifying at least one based on largest convex hull.

---
+++ autoMSK_functions/CS_pelvis_Kai2014.m
@@ -0,0 +1,180 @@
+%-------------------------------------------------------------------------%
+% Copyright (c) 2020 Modenese L.                                          %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         % 
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+%                                                                         %
+%    Author: Luca Modenese                                                %
+%    email:    l.modenese@imperial.ac.uk                                  % 
+% ----------------------------------------------------------------------- %
+function [ CS, JCS, PelvisBL] = CS_pelvis_Kai2014(Pelvis, result_plots, debug_plots, label_switch, in_mm)
+
+if nargin<2; result_plots=1; end
+if nargin<3;     debug_plots = 0;  end
+if nargin<4;     label_switch = 1;  end
+if nargin<5;     in_mm = 1;  end
+if in_mm == 1;     dim_fact = 0.001;  else;  dim_fact = 1; end
+
+% Initial guess of CS direction [JB]
+% RISB2Glob_guess: principal inertial axes named as ISB (guess)
+[ ~, RISB2Glob_guess] = pelvis_get_correct_first_CS(Pelvis);
+
+% Get eigen vectors V_all and volumetric center
+[eigVctrs, CenterVol, InertiaMatrix, D ] =  TriInertiaPpties(Pelvis);
+
+%------------------------------------
+% TODO: develop other way of identifying the axes direction.
+% there is always an eigenvector smaller than the other
+% [~, ind_v] = min(diag(D));
+% v = circshift([1, 2, 3], ind_v-1);
+% y_pelvis_in_global = eigVctrs(:,ind_v);
+% z_pelvis_in_global = eigVctrs(:,v(1));
+% x_pelvis_in_global = eigVctrs(:,v(2));
+%------------------------------------
+
+% clarifying that this rotation goes inertial to global
+RInert2Glob = eigVctrs;
+
+% aligning pelvis ref system to ISB one provided
+[~, ind_v] = max(abs(RInert2Glob'*RISB2Glob_guess), [], 1);
+
+% signs of axes for the largest component
+sign_v = sign(RInert2Glob'*RISB2Glob_guess);
+
+% rotation matrix from glob -> inertial axes named as ISB (guess)
+RotISB2Glob = eigVctrs(:,ind_v).*[sign_v(ind_v(1), 1),  sign_v(ind_v(2), 2), sign_v(ind_v(3), 3)];
+
+% generating a triangulated pelvis with axes directed as they would be in a
+% ISB reference system (Y upwards, X frontal etc)
+[ PelvisPseudoISB, ~ , ~ ] = TriChangeCS( Pelvis, RotISB2Glob, CenterVol);
+
+% In ISB reference system: right side if z>0, upwards if Y>0
+R_side_ind = PelvisPseudoISB.Points(:,3)>0;
+
+% identifying the height of point beween most distal point and section with
+% largest medio-lateral span.
+[min_dist_H, ~] = min(PelvisPseudoISB.Points(:,2)); % most dist
+[~, max_WD_R_ind] = max(PelvisPseudoISB.Points(:,3));
+[~, max_WD_L_ind] = min(PelvisPseudoISB.Points(:,3));
+% simplification: taking the height of the largest section as midpoint of
+% furthest points in the +Z and -Z
+max_WD_H = (PelvisPseudoISB.Points(max_WD_R_ind,2)+PelvisPseudoISB.Points(max_WD_L_ind,2))/2;
+% height of dividing plane as described by Kai et al. 
+H_div_plane = (max_WD_H+min_dist_H)/2;
+
+% indices of points below the dividing plane
+Dist_ind =  PelvisPseudoISB.Points(:,2)<H_div_plane;
+
+%---------------------------------------------
+% % possible improvements sectioning the triangulations
+% [Areas, Alt] = TriSliceObjAlongAxis(PelvisPseudoISB, [0 -1 0 ]', 1);
+% [Areas, Alt] = TriSliceObjAlongAxis(PelvisPseudoISB, [0 0  1]', 1);
+% figure; plot(Alt, Areas)
+%---------------------------------------------
+
+% identifying bony landmarks
+[~, RASIS_ind] = max(PelvisPseudoISB.Points(:,1).* R_side_ind);
+[~, LASIS_ind] = max(PelvisPseudoISB.Points(:,1).*~R_side_ind);
+[~, RPSIS_ind] = min(PelvisPseudoISB.Points(:,1).* R_side_ind);
+[~, LPSIS_ind] = min(PelvisPseudoISB.Points(:,1).*~R_side_ind);
+[~, RPS_ind]   = max(PelvisPseudoISB.Points(:,1).* R_side_ind.*Dist_ind);
+[~, LPS_ind]   = max(PelvisPseudoISB.Points(:,1).*~R_side_ind.*Dist_ind);
+
+if debug_plots == 1
+    quickPlotTriang(PelvisPseudoISB);
+    plot3(  PelvisPseudoISB.Points(Dist_ind,1),...
+            PelvisPseudoISB.Points(Dist_ind,2),...
+            PelvisPseudoISB.Points(Dist_ind,3))
+    plotDot(PelvisPseudoISB.Points(RPS_ind,:), 'k', 7)
+    plotDot(PelvisPseudoISB.Points(LPS_ind,:), 'k', 7)
+end
+
+% extract points on bone in medical images ref system
+[RASIS, LASIS, RPSIS, LPSIS, RPS, LPS] = deal(  Pelvis.Points(RASIS_ind,:), ...
+                                                Pelvis.Points(LASIS_ind,:), ...
+                                                Pelvis.Points(RPSIS_ind,:), ...
+                                                Pelvis.Points(LPSIS_ind,:), ...
+                                                Pelvis.Points(RPS_ind,:), ...
+                                                Pelvis.Points(LPS_ind,:)); 
+
+% check if bone landmarks are correctly identified or axes were incorrect
+% TEST: inter-ASIS distance must be larger than inter-PSIS
+if norm(RASIS-LASIS)<norm(RPSIS-LPSIS)
+    % inform user
+    disp('GIBOK_pelvis.')
+    disp('Inter-ASIS distance is shorter than inter-PSIS distance.')
+    disp('Likely error in guessing medical image axes. Flipping X-axis.')
+    % switch ASIS and PSIS
+    % temp variables
+    LPSIS_temp = RASIS;
+    RPSIS_temp = LASIS;
+    % assign asis
+    LASIS = RPSIS;
+    RASIS = LPSIS;
+    % update psis
+    LPSIS = LPSIS_temp;
+    RPSIS = RPSIS_temp;
+    % TODO: recalculate synphisis with X = -X
+end
+
+% defining the ref system (global)
+PelvisOr = (RASIS+LASIS)'/2.0;
+Z = normalizeV(RASIS-LASIS);
+temp_X = ((RASIS+LASIS)/2.0) - ((RPSIS+LPSIS)/2.0);
+pseudo_X = temp_X/norm(temp_X);
+Y = normalizeV(cross(Z, pseudo_X));
+X = normalizeV(cross(Y, Z));
+
+% segment reference system
+CS.CenterVol = CenterVol;
+CS.InertiaMatrix = InertiaMatrix;
+CS.Origin = CenterVol;
+CS.V = [X Y Z];
+
+% storing joint details
+JCS.ground_pelvis.V                 = [X Y Z];
+JCS.ground_pelvis.Origin            = PelvisOr;
+JCS.ground_pelvis.child_location    = PelvisOr*dim_fact;
+JCS.ground_pelvis.child_orientation = computeXYZAngleSeq(CS.V);
+
+% define hip_r parent
+JCS.hip_r.parent_orientation        = computeXYZAngleSeq(CS.V);
+
+% Export bone landmarks
+PelvisBL.RASIS     = RASIS; % in Pelvis ref
+PelvisBL.LASIS     = LASIS; % in Pelvis ref
+PelvisBL.RPSIS     = RPSIS; % in Pelvis ref
+PelvisBL.LPSIS     = LPSIS; % in Pelvis ref
+PelvisBL.RPS       = RPS;
+PelvisBL.LPS       = LPS;
+
+% debug plot
+if result_plots == 1
+    PlotTriangLight(Pelvis, CS, 1); hold on
+    quickPlotRefSystem(CS)
+    quickPlotRefSystem(JCS.ground_pelvis);
+    % plot markers
+    BLfields = fields(PelvisBL);
+    for nL = 1:numel(BLfields)
+        cur_name = BLfields{nL};
+        plotDot(PelvisBL.(cur_name), 'k', 7)
+        if label_switch==1
+            text(PelvisBL.(cur_name)(1),...
+                PelvisBL.(cur_name)(2),...
+                PelvisBL.(cur_name)(3),...
+                ['  ',cur_name],...
+                'VerticalAlignment', 'Baseline',...
+                'FontSize',8);
+        end
+    end
+end
+
+end
