f13207e47a4634e82822ec78882f9a37a1dc70d1
Luca Modenese <l.modenese@imperial.ac.uk>
2020-05-01 21:42:03 +0100

    implements ISB pelvis ref systm

---
+++ autoMSK_functions/CS_pelvis_ISB.m
@@ -0,0 +1,10 @@
+function V = CS_pelvis_ISB(RASIS, LASIS, RPSIS, LPSIS)
+% defining the ref system (global)
+% with origin at midpoint fo ASIS
+Z = normalizeV(RASIS-LASIS);
+temp_X = ((RASIS+LASIS)/2.0) - ((RPSIS+LPSIS)/2.0);
+pseudo_X = temp_X/norm(temp_X);
+Y = normalizeV(cross(Z, pseudo_X));
+X = normalizeV(cross(Y, Z));
+V = [X Y Z];
+end
