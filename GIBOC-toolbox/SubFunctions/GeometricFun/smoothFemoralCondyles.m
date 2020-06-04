2df528ac379a01df6965830c431e0aeea4699701
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-04 20:26:52 +0100

    renaming and moving from GIBOC to automsk

--- GIBOC-toolbox/SubFunctions/GeometricFun/smoothFemoralCondyles.m
+++
@@ -1,8 +0,0 @@
-% written by Luca Modenese based on GIBOK code
-function fullCondyle = smoothFemoralCondyles(EpiFem, PtsFullCondyle, CoeffMorpho)
-
-Condyle = TriReduceMesh(EpiFem, [], PtsFullCondyle );
-Condyle2 = TriCloseMesh(EpiFem, Condyle, 5*CoeffMorpho);
-fullCondyle = TriOpenMesh(EpiFem, Condyle2, 15*CoeffMorpho);
-
-end
---
+++ autoMSK_functions/femur_smoothCondyles.m
@@ -0,0 +1,8 @@
+% written by Luca Modenese based on GIBOK code
+function fullCondyle = femur_smoothCondyles(EpiFem, PtsFullCondyle, CoeffMorpho)
+
+Condyle = TriReduceMesh(EpiFem, [], PtsFullCondyle );
+Condyle2 = TriCloseMesh(EpiFem, Condyle, 5*CoeffMorpho);
+fullCondyle = TriOpenMesh(EpiFem, Condyle2, 15*CoeffMorpho);
+
+end
