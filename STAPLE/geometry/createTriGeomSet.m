027d230bf61412ed22588568982f2aba4fc291ac
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-06 15:11:14 +0100

    (fix) don't create empty strct fields for geometries that are not available. Breaks the visualization geometry writer

--- STAPLE/geometry/createTriGeomSet.m
+++ STAPLE/geometry/createTriGeomSet.m
@@ -9,7 +9,13 @@ tic
 for nb = 1:numel(aTriGeomList)
     cur_tri_name = aTriGeomList{nb};
     cur_tri_geom_file = fullfile(geom_file_folder, cur_tri_name);
-    triGeomSet.(cur_tri_name) = load_mesh(cur_tri_geom_file);
+    cur_tri_geom = load_mesh(cur_tri_geom_file);
+    if isempty(cur_tri_geom)
+        % skip building the field if there was no geometry
+        continue
+    else
+        triGeomSet.(cur_tri_name) = cur_tri_geom;
+    end
 end
 % tell user all went well
 disp(['Geometry set of triangulations created in ', num2str(toc), ' s']);
