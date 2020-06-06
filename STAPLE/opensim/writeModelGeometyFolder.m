5200101b6eb87694c1df70933c71b020d36296e9
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-06 14:44:02 +0100

    (feat) write model visualization geometry for immediate use of the model in teh GUI

---
+++ STAPLE/opensim/writeModelGeometyFolder.m
@@ -0,0 +1,28 @@
+function writeModelGeometyFolder(aTriGeomBoneSet, aGeomFolder, aFileFormat)
+
+% all formats are ASCII, as required by the OpenSim visualized
+if nargin<3; aFileFormat='obj'; end
+if nargin<2; aGeomFolder='.'; end
+
+% create geometry file if not existing
+if ~isfolder(aGeomFolder); mkdir(aGeomFolder); end
+
+% ensure lower case fileformat
+aFileFormat = lower(aFileFormat);
+
+% how many bones to print?
+bone_names = fields(aTriGeomBoneSet);
+N_bones = numel(bone_names);
+
+for nb = 1:N_bones
+    cur_bone_name = bone_names{nb};
+    cur_tri = aTriGeomBoneSet.(cur_bone_name);
+    switch aFileFormat
+        case 'obj'
+            writeOBJfile(cur_tri, fullfile(aGeomFolder, [cur_bone_name,'.obj']));
+        case 'stl'
+            % requires Matlab>2018b
+            stlwrite(cur_tri, fullfile(aGeomFolder, [cur_bone_name,'.stl']));
+        otherwise
+    end
+end
