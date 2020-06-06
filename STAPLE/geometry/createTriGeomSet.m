9c92da939f90ef95abb8066280eaa34b2bb688d5
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-06 11:15:48 +0100

    add importing time printout

--- STAPLE/geometry/createTriGeomSet.m
+++ STAPLE/geometry/createTriGeomSet.m
@@ -5,9 +5,13 @@
 %    email:    l.modenese@imperial.ac.uk                                  %
 % ----------------------------------------------------------------------- %
 function triGeomSet = createTriGeomSet(aTriGeomList, geom_file_folder)
+tic
 for nb = 1:numel(aTriGeomList)
     cur_tri_name = aTriGeomList{nb};
     cur_tri_geom_file = fullfile(geom_file_folder, cur_tri_name);
     triGeomSet.(cur_tri_name) = load_mesh(cur_tri_geom_file);
 end
+% tell user all went well
+disp(['Geometry set of triangulations created in ', num2str(toc), ' s']);
+
 end
