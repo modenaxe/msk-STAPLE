c0458afb3e6eab7fcbc70d0189bc0ef65aad4223
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-23 09:37:35 +0100

    add header

--- STAPLE/algorithms/private/GIBOC_femur_fitSphere2FemHead.m
+++ STAPLE/algorithms/private/GIBOC_femur_fitSphere2FemHead.m
@@ -1,3 +1,21 @@
+%-------------------------------------------------------------------------%
+% Copyright (c) 2020 Modenese L., Renault, J-B                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+%                                                                         %
+%    Author:   Jean-Baptiste Renault, Modenese Luca					      %
+% ----------------------------------------------------------------------- %
+
+
 function [CSs, FemHead] = GIBOC_femur_fitSphere2FemHead(ProxFem, CSs, CoeffMorpho, debug_plots)
     
 if nargin < 4; debug_plots = 0; end
