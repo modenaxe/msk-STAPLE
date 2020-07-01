f93abf1573d31192a170f4095ef5a96614a2e71c
Luca Modenese <luca.modenese@gmail.com>
2020-07-01 15:52:18 +0100

    added help header

--- STAPLE/geometry/findLandmarkCoords.m
+++ STAPLE/geometry/findLandmarkCoords.m
@@ -1,14 +1,40 @@
+% FINDLANDMARKCOORDS Pick points from a point cloud using string keywords.
+%
+%   [BL, BL_ind] = findLandmarkCoords(points, axis_name, direction)
+%
+% Inputs:
+%   points - indicates a point cloud, i.e. a matrix of [N_point, 3]
+%       dimensions.
+%
+%   axis_name - specifies the axis where the min/max operator will be
+%       applied. Assumes values: 'x', 'y' or 'z', corresponding to first,
+%       second or third column of the points input.
+%
+%   operator - specifies which point to pick in the specified axis.
+%       Currently assumes values: 'min' or 'max', meaning smalles or
+%       largest value in that direction.
+%
+% Outputs:
+%   BL - coordinates of the point identified in the point cloud following
+%       the directions provided as input. 
+%
+%   BL_ind - index of the point identified in the point cloud following the
+%       direction provided as input.
+% 
+% Example:
+% getBonyLandmark(pcloud,'max','z') asks to pick the point in the pcloud 
+% with largest z coordinate. If pcloud was a right distal femur triangulation
+% with points expressed in the reference system of the International
+% Society of Biomechanics, this could be the lateral femoral epicondyle.
+%
+% See also GETBONELANDMARKLIST, LANDMARKTRIGEOMBONE.
+%
 % ----------------------------------------------------------------------- %
-%    Author:   Luca Modenese,  2015                                       %
+%    Author:   Luca Modenese,  2020                                       %
 %    email:    l.modenese@imperial.edu.au                                 % 
 % ----------------------------------------------------------------------- %
-%
-% This function makes easier to pick points from a certain point cloud by
-% specifying only the direction and the position through strings.
-% For instance, the function call getBonyLandmark(v_oct,'max','x') reads as
-% 'pick the point of v_oct with maximum x coordinate'.
 
-function [BL, BL_ind] = findLandmarkCoords(points, axis_name, direction)
+function [BL, BL_ind] = findLandmarkCoords(points, axis_name, operator)
 
 % TO DO ADD CHECK FOR DESCRIPT AND DIRECTION AS STRINGS
 % interpreting direction of search
@@ -24,7 +50,7 @@ switch axis_name
 end
 
 % interpreting description of search
-switch direction
+switch operator
     case 'max'
         [~, BL_ind] = max(points(:,dir_ind));
     case 'min'
