d20018371cfa6d0209ab54ef9a105384ff4f7a6c
Luca Modenese <luca.modenese@gmail.com>
2020-07-13 09:50:31 +0200

    (feat) plot bone landmarks and write marker label name on all bone landmarks in input struct

---
+++ STAPLE/geometry/plotBoneLandmarks.m
@@ -0,0 +1,37 @@
+% PLOTBONELANDMARKS Add point plots, and if required, labels to bone
+% landmarks identified throught the STAPLE analyses.
+%
+%   plotBoneLandmarks(BLStruct, label_switch)
+%
+% Inputs:
+%   BLStruct - a MATLAB structure with fields having as fields the name of 
+%       the bone landmarks and as values their coordinates (in global
+%       reference system).
+%
+%   label_switch - a binary switch that indicates if the bone landmark
+%       names will be added or not (as text) to the plot.
+%
+% Outputs:
+%   none - the points are plotted on the current axes.
+%
+% See also PLOTDOT.
+%
+%-------------------------------------------------------------------------%
+%  Author:   Luca Modenese
+%  Copyright 2020 Luca Modenese
+%-------------------------------------------------------------------------%
+function plotBoneLandmarks(BLStruct, label_switch)
+    BLfields = fields(BLStruct);
+    for nL = 1:numel(BLfields)
+        cur_name = BLfields{nL};
+        plotDot(BLStruct.(cur_name), 'k', 7)
+        if label_switch==1
+            text(BLStruct.(cur_name)(1),...
+                BLStruct.(cur_name)(2),...
+                BLStruct.(cur_name)(3),...
+                ['  ',cur_name],...
+                'VerticalAlignment', 'Baseline',...
+                'FontSize',8);
+        end
+    end
+end
