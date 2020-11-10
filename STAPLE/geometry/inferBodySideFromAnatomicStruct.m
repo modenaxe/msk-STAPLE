41ea3e7b5a40ecede949869a6faee5cd2f5f1a10
Luca Modenese <luca.modenese@gmail.com>
2020-11-10 10:13:36 +0000

    func that guesses body side from structure of anatomical objects

---
+++ STAPLE/geometry/inferBodySideFromAnatomicStruct.m
@@ -0,0 +1,42 @@
+function guessed_side = inferBodySideFromAnatomicStruct(anat_struct)
+% check using the bone names
+fields_side = fields(anat_struct);
+body_set = {'femur', 'tibia', 'talus', 'calcn'};
+ng_b = 1;
+guess_side_b = '';
+for nb = 1:length(body_set)
+    cur_body = body_set{nb};
+    ind_b = strncmp( fields_side, cur_body, length(cur_body));
+    if sum(ind_b)>0
+        guess_side_b(ng_b) = fields_side{ind_b}(end);
+        ng_b = ng_b+1;
+    else
+        continue
+    end
+end
+
+% check using the joint names
+joint_set = {'hip', 'knee', 'ankle', 'subtalar'};
+ng_j = 1;
+guess_side_j = '';
+for nj = 1:length(joint_set)
+    cur_joint = joint_set{nj};
+    ind_j = strncmp( fields_side, cur_joint, length(cur_joint));
+    if sum(ind_j)>0
+        guess_side_j(ng_j) = fields_side{ind_j}(end);
+        ng_j = ng_j+1;
+    else
+        continue
+    end
+end
+
+% composed vectors
+combined_guessed = [guess_side_b, guess_side_j];
+
+if strcmpi(unique(combined_guessed), 'r')
+    guessed_side = 'r';
+elseif strcmpi(unique(combined_guessed), 'l')
+    guessed_side = 'l';
+else
+    error('guessBodySideFromAnatomicStruct.m Error: it was not possible to infer the body side. Please specify it manually in this occurrance.');
+end
