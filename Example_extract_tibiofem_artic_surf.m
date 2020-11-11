42bf2f2db35e60b0225ec1573f45bddd90c4893e
Luca Modenese <luca.modenese@gmail.com>
2020-11-11 09:07:07 +0000

    change res folder

--- Example_extract_tibiofem_artic_surf.m
+++ Example_extract_tibiofem_artic_surf.m
@@ -34,7 +34,7 @@ dataset_set = {'LHDL_CT', 'TLEM2_CT', 'ICL_MRI'};
 bones_list = {'femur_r','tibia_r'};
 
 % folder where the articulalar surfaces will be stored
-output_artic_surf_folder = './Articular_surfaces';
+output_artic_surf_folder = './articular_surfaces/example_tibiofem';
 %--------------------------------------
 
 for n_d = 1:numel(dataset_set)
