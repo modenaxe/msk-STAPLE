a1ca4d0ba0e3b9642d778ccab57000faa706976a
Luca Modenese <luca.modenese@gmail.com>
2020-11-12 17:14:00 +0000

    handles left side

--- STAPLE/anthropometry/assignMassPropsToSegments.m
+++ STAPLE/anthropometry/assignMassPropsToSegments.m
@@ -16,7 +16,10 @@
 %   subj_mass - the total mass of the individual of which we are building a
 %       model, in Kg. Required for scaling the mass properties of the
 %       generic gati2392 model.
-% 
+%
+%   side_raw - generic string identifying a body side. 'right', 'r', 'left'
+%       and 'l' are accepted inputs, both lower and upper cases.
+%
 % Outputs:
 %   osimModel - the OpenSim model with the personalised mass properties.
 %
@@ -27,29 +30,60 @@
 %  Author:   Luca Modenese
 %  Copyright 2020 Luca Modenese
 %-------------------------------------------------------------------------%
-function osimModel = assignMassPropsToSegments(osimModel, JCS, subj_mass)
+%
+% TODO: remove dependance from osimVec3FromArray of the API
+
+function osimModel = assignMassPropsToSegments(osimModel, JCS, subj_mass, side_raw)
+
+% opensim libraries
+import org.opensim.modeling.*
+
+% TODO: rewrite in a more clever way
+% setting defaults
+if nargin<4
+    side = inferBodySideFromAnatomicStruct(JCS);
+else
+    % get sign correspondent to body side
+    [~, side] = bodySide2Sign(side_raw);
+end
+
+femur_name = ['femur_', side];
+tibia_name = ['tibia_', side];
+talus_name = ['talus_', side];
+calcn_name = ['calcn_', side];
+hip_name   = ['hip_',side];
+knee_name  = ['knee_',side];
+ankle_name = ['ankle_', side];
+toes_name  = ['toes_',side];
+
+disp('------------------------');
+disp('  UPDATING MASS PROPS   ')
+disp('------------------------');
 
 % MADE A MESS WITH THE DIMENSIONS OF THE JCS.origin....TO FIX!
 %---------------------------------------------
 % compute lengths of segments from the bones
-thigh_axis = JCS.femur_r.hip_r.Origin-JCS.femur_r.knee_r.Origin;
+thigh_axis = JCS.(femur_name).(hip_name).Origin-JCS.(femur_name).(knee_name).Origin;
 thigh_L = norm(thigh_axis);
-shank_axis = JCS.femur_r.knee_r.Origin-JCS.talus_r.ankle_r.Origin';
+shank_axis = JCS.(femur_name).(knee_name).Origin-JCS.(talus_name).(ankle_name).Origin';
 shank_L = norm(shank_axis);
-foot_axis = JCS.talus_r.ankle_r.Origin'-JCS.calcn_r.toes_r.Origin;
+foot_axis = JCS.(talus_name).(ankle_name).Origin'-JCS.(calcn_name).(toes_name).Origin;
 foot_L = norm(foot_axis);
 %---------------------------------------------
 
 % compute COM positions using coefficients from Winter 2015 (book)
-thigh_COM = thigh_L*0.567*thigh_axis/thigh_L+JCS.femur_r.knee_r.Origin;
-shank_COM = shank_L*0.567*shank_axis/shank_L+JCS.talus_r.ankle_r.Origin';
-calcn_COM = foot_L*0.5*foot_axis/foot_L+JCS.calcn_r.toes_r.Origin;
+thigh_COM = thigh_L*0.567*thigh_axis/thigh_L + JCS.(femur_name).(knee_name).Origin;
+shank_COM = shank_L*0.567*shank_axis/shank_L + JCS.(talus_name).(ankle_name).Origin';
+calcn_COM = foot_L*0.5*foot_axis/foot_L      + JCS.(calcn_name).(toes_name).Origin;
+
 % set the COM
-osimModel.getBodySet().get('femur_r').setMassCenter(osimVec3FromArray(thigh_COM/1000));
-osimModel.getBodySet().get('tibia_r').setMassCenter(osimVec3FromArray(shank_COM/1000));
-osimModel.getBodySet().get('calcn_r').setMassCenter(osimVec3FromArray(calcn_COM/1000));
+disp('Updating centre of mass position (Winter 2015)...')
+osimModel.getBodySet().get(femur_name).setMassCenter(ArrayDouble.createVec3(thigh_COM/1000));
+osimModel.getBodySet().get(tibia_name).setMassCenter(ArrayDouble.createVec3(shank_COM/1000));
+osimModel.getBodySet().get(calcn_name).setMassCenter(ArrayDouble.createVec3(calcn_COM/1000));
 
 % map gait2392 properties to the model segments as an initial value
+disp('Mapping segment masses and inertias from gait2392/Rajagopal model.')
 osimModel = mapGait2392MassPropToModel(osimModel);
 
 % opensim model total mass (consistent in gait2392 and Rajagopal)
@@ -60,6 +94,8 @@ gait2392_tot_mass = 11.777 + 2*(9.3014+3.7075+0.1+1.25+0.2166+0.0862)+34.2366;
 coeff = subj_mass/gait2392_tot_mass;
 
 % scale gait2392 mass properties to the individual subject
+disp('Scaling inertial properties to assigned body weight...')
 scaleMassProps(osimModel, coeff);
 
+disp('Done.')
 end
