5034e9445901bc2b606b39bd76db5b1c01c1b8b1
Luca Modenese <luca.modenese@gmail.com>
2020-11-17 18:00:12 +0000

    fix figure id

--- STAPLE/algorithms/STAPLE_pelvis.m
+++ STAPLE/algorithms/STAPLE_pelvis.m
@@ -123,7 +123,7 @@ PelvisBL.SYMP     = SYMP;
 % debug plot
 label_switch = 1;
 if result_plots == 1
-    figure('Name', ['GIBOC | bone: pelvis | side: ', side_low])
+    figure('Name', ['STAPLE | bone: pelvis | side: ', side_low])
     plotTriangLight(Pelvis, CS, 0); hold on
     quickPlotRefSystem(CS);
     quickPlotRefSystem(JCS.ground_pelvis);
