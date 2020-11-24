1d799167f88572a26f97ac5bb7cccceca51da414
Luca Modenese <luca.modenese@gmail.com>
2020-11-24 18:25:07 +0000

    comment

--- STAPLE/geometry/compileListOfJointsInJCSStruct.m
+++ STAPLE/geometry/compileListOfJointsInJCSStruct.m
@@ -1,3 +1,26 @@
+% COMPILELISTOFJOINTSINJCSSTRUCT Create a list of joints that can be 
+% modelled from the structure that results from morphological analysis.
+%
+%   joint_list = compileListOfJointsInJCSStruct(JCS)
+%
+% Inputs:
+%   JCS - structure with the joint parameters produced by the morphological 
+%       analyses of processTriGeomBoneSet.m. Not all listed joints are
+%       actually modellable, in the sense that the parent and child
+%       reference systems might not be present, the model might be
+%       incomplete etc.
+%
+% Outputs:
+%   joint_list - a cell array with a list of unique elements. Each element
+%       is the name of a joint present in the JCS structure.
+%
+%
+% See also CREATEOPENSIMMODELJOINTS, PROCESSTRIGEOMBONESET.
+%
+%-------------------------------------------------------------------------%
+%  Author:   Luca Modenese
+%  Copyright 2020 Luca Modenese
+%-------------------------------------------------------------------------%
 function joint_list = compileListOfJointsInJCSStruct(JCS)
 bodies_list = fields(JCS)';
 joint_list = {};
