b9da0124449f1e9f98f3f6bba60d52eb23b29eb2
Luca Modenese <luca.modenese@gmail.com>
2021-01-07 12:24:42 +0000

    upd license and descriptions

--- Example_bilateral_model.m
+++ Example_bilateral_model.m
@@ -1,28 +1,11 @@
 %-------------------------------------------------------------------------%
 % Copyright (c) 2020 Modenese L.                                          %
-%                                                                         %
-% Licensed under the Apache License, Version 2.0 (the "License");         %
-% you may not use this file except in compliance with the License.        %
-% You may obtain a copy of the License at                                 %
-% http://www.apache.org/licenses/LICENSE-2.0.                             %
-%                                                                         %
-% Unless required by applicable law or agreed to in writing, software     %
-% distributed under the License is distributed on an "AS IS" BASIS,       %
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
-% implied. See the License for the specific language governing            %
-% permissions and limitations under the License.                          %
-%                                                                         %
 %    Author:   Luca Modenese,  2020                                       %
 %    email:    l.modenese@imperial.ac.uk                                  %
 % ----------------------------------------------------------------------- %
 % This example demonstrates how to setup a STAPLE workflow to
 % automatically generate a complete kinematic model of the right legs of
-% the anatomical datasets used in the manuscript:
-% Modenese, Luca, and Jean-Baptiste Renault. "Automatic Generation of
-% Personalised Skeletal Models of the Lower Limb from Three-Dimensional
-% Bone Geometries." bioRxiv (2020).
-% https://www.biorxiv.org/content/10.1101/2020.06.23.162727v2
-% and included in the bone_dataset folder.
+% the anatomical datasets TLEM2_MRI and JIA_MRI.
 % ----------------------------------------------------------------------- %
 clear; clc; close all
 addpath(genpath('STAPLE'));
