d07627a43bf27d5cdec7f82e7a008ab332b69340
Luca Modenese <luca.modenese@gmail.com>
2021-01-13 15:40:56 +0000

    upd code for incompl models

--- STAPLE/anthropometry/assignMassPropsToSegments.m
+++ STAPLE/anthropometry/assignMassPropsToSegments.m
@@ -37,6 +37,8 @@ function osimModel = assignMassPropsToSegments(osimModel, JCS, subj_mass, side_r
 % opensim libraries
 import org.opensim.modeling.*
 
+av_bodies = fields(JCS);
+
 % TODO: rewrite in a more clever way
 % setting defaults
 if nargin<4
@@ -61,33 +63,51 @@ disp('------------------------');
 
 % MADE A MESS WITH THE DIMENSIONS OF THE JCS.origin....TO FIX!
 %---------------------------------------------
-% compute lengths of segments from the bones
-thigh_axis = JCS.(femur_name).(hip_name).Origin-JCS.(femur_name).(knee_name).Origin;
-thigh_L = norm(thigh_axis);
-shank_axis = JCS.(femur_name).(knee_name).Origin-JCS.(talus_name).(ankle_name).Origin';
-shank_L = norm(shank_axis);
-foot_axis = JCS.(talus_name).(ankle_name).Origin'-JCS.(calcn_name).(toes_name).Origin;
-foot_L = norm(foot_axis);
-%---------------------------------------------
 
-% compute COM positions using coefficients from Winter 2015 (book)
-thigh_COM = thigh_L*0.567*thigh_axis/thigh_L + JCS.(femur_name).(knee_name).Origin;
-shank_COM = shank_L*0.567*shank_axis/shank_L + JCS.(talus_name).(ankle_name).Origin';
-calcn_COM = foot_L*0.5*foot_axis/foot_L      + JCS.(calcn_name).(toes_name).Origin;
 
-% set the COM
+% compute lengths of segments from the bones and COM positions using 
+% coefficients from Winter 2015 (book)
+% badly written!
+
+% upper leg
 disp('Updating centre of mass position (Winter 2015)...')
-osimModel.getBodySet().get(femur_name).setMassCenter(ArrayDouble.createVec3(thigh_COM/1000));
-osimModel.getBodySet().get(tibia_name).setMassCenter(ArrayDouble.createVec3(shank_COM/1000));
-osimModel.getBodySet().get(calcn_name).setMassCenter(ArrayDouble.createVec3(calcn_COM/1000));
+if isfield(JCS, femur_name)
+    thigh_axis = JCS.(femur_name).(hip_name).Origin-...
+                 JCS.(femur_name).(knee_name).Origin;
+    thigh_L = norm(thigh_axis);
+    thigh_COM = thigh_L*0.567*thigh_axis/thigh_L + JCS.(femur_name).(knee_name).Origin;
+    
+    osimModel.getBodySet().get(femur_name).setMassCenter(ArrayDouble.createVec3(thigh_COM/1000));
+    
+    % shank
+    if isfield(JCS, talus_name)
+        shank_axis = JCS.(femur_name).(knee_name).Origin...
+                    -JCS.(talus_name).(ankle_name).Origin';
+        shank_L = norm(shank_axis);
+        shank_COM = shank_L*0.567*shank_axis/shank_L + JCS.(talus_name).(ankle_name).Origin';
+        
+        osimModel.getBodySet().get(tibia_name).setMassCenter(ArrayDouble.createVec3(shank_COM/1000));
+        
+        % foot
+        if isfield(JCS, calcn_name)
+            foot_axis = JCS.(talus_name).(ankle_name).Origin'...
+                        -JCS.(calcn_name).(toes_name).Origin;
+            foot_L = norm(foot_axis);
+            calcn_COM = foot_L*0.5*foot_axis/foot_L      + JCS.(calcn_name).(toes_name).Origin;
+            
+            osimModel.getBodySet().get(calcn_name).setMassCenter(ArrayDouble.createVec3(calcn_COM/1000));
+        end
+    end
+end
+%---------------------------------------------
 
 % map gait2392 properties to the model segments as an initial value
 disp('Mapping segment masses and inertias from gait2392/Rajagopal model.')
 osimModel = mapGait2392MassPropToModel(osimModel);
 
 % opensim model total mass (consistent in gait2392 and Rajagopal)
-% This is: pelvis + 2*(fem+tibia+talus+foot+toes+patella)+torso    
-gait2392_tot_mass = 11.777 + 2*(9.3014+3.7075+0.1+1.25+0.2166+0.0862)+34.2366;
+MP = gait2392MassProps('full_body');
+gait2392_tot_mass = MP.mass;
 
 % calculate mass ratio of subject mass and gait2392 mass
 coeff = subj_mass/gait2392_tot_mass;
