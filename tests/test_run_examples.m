dd18c9e9178f409294be19e79966b5e161ff8e96
Luca Modenese <luca.modenese@gmail.com>
2021-01-13 15:33:46 +0000

    comments

--- tests/test_run_examples.m
+++ tests/test_run_examples.m
@@ -3,17 +3,18 @@
 %    Author: Luca Modenese                                                %
 %    email:    l.modenese@imperial.ac.uk                                  % 
 % ----------------------------------------------------------------------- %
+% get list of examples
 
 ex_list = dir('../Example*.m');
 run_str = '';
+% build string to execute (cannot simply run through them otherwise the
+% clear command in the example scripts deletes also ex_list).
 for n_ex = 1:numel(ex_list)
     if ~isfolder(ex_list(n_ex).name)
-%         evalin('caller', ['run(''',fullfile(ex_list(n_ex).folder, ex_list(n_ex).name),''')'])
         run_str = [run_str, 'run(''',fullfile(ex_list(n_ex).folder, ex_list(n_ex).name),''');'];
-        
     else
         continue
     end
 end
-        
+% executes all scripts as listed
 eval(run_str)
