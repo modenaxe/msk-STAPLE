bb5c63c5f04fd1fb86419daf27a01cfae12d6163
renaultJB <j.baptiste.renault@gmail.com>
2021-02-19 23:13:33 +0100

    more comments

--- STAPLE/GIBOC-core/SubFunctions/GeometricFun/PlanPolygonCentroid3D.m
+++ STAPLE/GIBOC-core/SubFunctions/GeometricFun/PlanPolygonCentroid3D.m
@@ -1,35 +1,47 @@
-function [ Centroid, Area ] = PlanPolygonCentroid3D( Pts )
-%PlanPolygonCentroid3D Find the centroid of a 2D Polygon, decribed by its
-%boundary ( a close curve) in a 3D space.
+% PLANPOLYGONCENTROID3D Find the centroid of a 2D Polygon, decribed by its
+% boundary ( a close curve) in a 3D space.
 % Works with arbitrary shapes (convex or not)
+%
+% [ Centroid, Area ] = PlanPolygonCentroid3D( Pts )
+%
+% Inputs:
+%   Pts - A close curve in 3D space [nx3] matrix.
+%
+% Outputs:
+%   Centroid - a 2x3 or 2x2 matrix of the pair of closest point.
+%   Area - Area of the curves.
+%
+% -------------------------------------------------------------------------
+%  Author:   Jean-Baptiste Renault
+%  Copyright 2020 Jean-Baptiste Renault
+%-------------------------------------------------------------------------%
+function [ Centroid, Area ] = PlanPolygonCentroid3D( Pts )
 
+    % Close curve if not already closed
+    if Pts(1, :) ~= Pts(end, :)
+        Pts(end+1, :) = Pts(1, :);
+    end
 
-if Pts(1,:) ~= Pts(end,:)
-    Pts(end+1,:) = Pts(1,:);
-end
+    % Initial Guess of center
+    Center0 = mean(Pts(1:end-1, :));
 
-% Initial Guess of center
-Center0 = mean(Pts(1:end-1,:));
+    % Middle Point of each polygon side
+    PtsMiddle = Pts(1:end-1, :) + diff(Pts)/2;
 
-% Middle Point of each polygon side
-PtsMiddle = Pts(1:end-1,:) + diff(Pts)/2;
+    % Get the centroid of each points connected
+    TrianglesCentroid  = PtsMiddle - 1/3*bsxfun(@minus, PtsMiddle, Center0);
 
-% Get the centroid of each points connected
-TrianglesCentroid  = PtsMiddle - 1/3*bsxfun(@minus,PtsMiddle,Center0);
+    %Get the area of each triangles
+    [V,~] = eig(cov(Pts(1:end-1, :)));
+    n = V(:, 1); % normal to polygon plan
 
-%Get the area of each triangles
-[V,~] = eig(cov(Pts(1:end-1,:)));
-n= V(:,1); % normal to polygon plan
+    TrianglesArea = 1/2*cross(diff(Pts), -bsxfun(@minus, Pts(1:end-1,:), Center0))*n;
 
-TrianglesArea =1/2*cross(diff(Pts),-bsxfun(@minus,Pts(1:end-1,:),Center0))*n;
+    % Barycenter of triangles
+    Centroid = sum(TrianglesCentroid.*repmat(TrianglesArea, 1, 3))/sum(TrianglesArea);
 
-% Barycenter of triangles
-Centroid = sum(TrianglesCentroid.*repmat(TrianglesArea,1,3))/sum(TrianglesArea);
-
-if nargout>1
-    Area = abs(sum(TrianglesArea));
-end
-
-
+    if nargout>1
+        Area = abs(sum(TrianglesArea));
+    end
 end
 
