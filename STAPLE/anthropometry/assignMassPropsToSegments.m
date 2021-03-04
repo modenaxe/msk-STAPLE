38c78d248883eed167370d54b4e1c7f5989046c2
Luca Modenese <luca.modenese@gmail.com>
2021-03-04 18:16:56 +0000

    upd for Origin dim [3x1]

--- STAPLE/anthropometry/assignMassPropsToSegments.m
+++ STAPLE/anthropometry/assignMassPropsToSegments.m
@@ -61,40 +61,36 @@ disp('------------------------');
 disp('  UPDATING MASS PROPS   ')
 disp('------------------------');
 
-% MADE A MESS WITH THE DIMENSIONS OF THE JCS.origin....TO FIX!
-%---------------------------------------------
-
-
 % compute lengths of segments from the bones and COM positions using 
 % coefficients from Winter 2015 (book)
 % badly written!
 
-% upper leg
+% Keep in mind that all Origin fields have [3x1] dimensions
 disp('Updating centre of mass position (Winter 2015)...')
 if isfield(JCS, femur_name)
-    thigh_axis = JCS.(femur_name).(hip_name).Origin-...
-                 JCS.(femur_name).(knee_name).Origin;
+    % compute thigh length
+    thigh_axis = JCS.(femur_name).(hip_name).Origin-JCS.(femur_name).(knee_name).Origin;
     thigh_L = norm(thigh_axis);
-    thigh_COM = thigh_L*0.567*thigh_axis/thigh_L + JCS.(femur_name).(knee_name).Origin;
-    
+    thigh_COM = thigh_L*0.567 * (thigh_axis/thigh_L) + JCS.(femur_name).(knee_name).Origin;
+    % assign  thigh COM
     osimModel.getBodySet().get(femur_name).setMassCenter(ArrayDouble.createVec3(thigh_COM/1000));
     
     % shank
     if isfield(JCS, talus_name)
-        shank_axis = JCS.(femur_name).(knee_name).Origin...
-                    -JCS.(talus_name).(ankle_name).Origin';
+        % compute shank length
+        shank_axis = JCS.(femur_name).(knee_name).Origin-JCS.(talus_name).(ankle_name).Origin;
         shank_L = norm(shank_axis);
-        shank_COM = shank_L*0.567*shank_axis/shank_L + JCS.(talus_name).(ankle_name).Origin';
-        
+        shank_COM = shank_L*0.567 * (shank_axis/shank_L) + JCS.(talus_name).(ankle_name).Origin;
+        % assign shank COM
         osimModel.getBodySet().get(tibia_name).setMassCenter(ArrayDouble.createVec3(shank_COM/1000));
         
         % foot
         if isfield(JCS, calcn_name)
-            foot_axis = JCS.(talus_name).(ankle_name).Origin'...
-                        -JCS.(calcn_name).(toes_name).Origin;
+            % compute foot length
+            foot_axis = JCS.(talus_name).(ankle_name).Origin-JCS.(calcn_name).(toes_name).Origin;
             foot_L = norm(foot_axis);
             calcn_COM = foot_L*0.5*foot_axis/foot_L      + JCS.(calcn_name).(toes_name).Origin;
-            
+            % assign foot COM
             osimModel.getBodySet().get(calcn_name).setMassCenter(ArrayDouble.createVec3(calcn_COM/1000));
         end
     end
