841023b7cbecac3111221a7f88e1f4b803ee8085
Luca Modenese <luca.modenese@gmail.com>
2021-04-16 12:43:41 +0100

    fix BCS on feet segment. Requires more testing

--- STAPLE/algorithms/STAPLE_toes.m
+++ STAPLE/algorithms/STAPLE_toes.m
@@ -2,7 +2,7 @@
 %  Author:   Luca Modenese 
 %  Copyright 2020 Luca Modenese 
 %-------------------------------------------------------------------------%
-function [CS, JCS, ToesBL] = STAPLE_toes(toesTri, side_raw, result_plots,  debug_plots, in_mm)
+function [BCS, JCS, ToesBL] = STAPLE_toes(toesTri, side_raw, result_plots,  debug_plots, in_mm)
 
 % results/debug plot default
 if nargin<3;     result_plots =1;  end
@@ -30,30 +30,43 @@ disp('Initializing method...')
 
 % 1. Indentify initial CS of the foot
 % Get eigen vectors V_all of the Talus 3D geometry and volumetric center
-[ V_all, CenterVol ] = TriInertiaPpties( toesTri );
+[ V_all, CenterVol, ~, D ] = TriInertiaPpties( toesTri );
 
-% THIS IS USED IN jointDefinitions_Modenese2018.m
-CS.V = V_all;
-CS.Origin = CenterVol;
+% ASSUMPTIONS - TO VERIFY:
+% 1) largest eigenvalue is medial-lateral
+% 2) smaller eigenvalue is vertical direction.
+[~, I] = sort(diag(D));
+z = V_all(:,I(1));
+y = V_all(:,I(3));
+x = normalizeV(cross(y,z));
+% being unsure is this will handle the correct directions, I re-cross
+z = normalizeV(cross(x,y));
 
-% define toes joint
+% required for plotting and transforming geometry, even if not 100% tested
+% this will not influence model results except joint reaction at
+% calcn-toes joint
+BCS.V = [x, y, z];
+BCS.Origin = CenterVol;
+
+% define toes joint (CURRENTLY NOT IMPLEMENTED)
 % JCS.(toes_name).Origin = ;
 % JCS.(toes_name).V = ;
 % JCS.(toes_name).parent_location = ;
-% JCS.(toes_name).parent_orientation = );
+% JCS.(toes_name).parent_orientation = ;
+
+% fields filled building the model
 JCS.(toes_name) = [];
 ToesBL = [];
 
 if result_plots == 1
     figure('Name', ['STAPLE | bone: toes | side: ', side_low])
     % plot the calcn triangulation
-    plotTriangLight(toesTri, CS, 0)
-    % Plot the inertia Axis & Volumic center
-%     quickPlotRefSystem(JCS.(toes_name))
-    
-    % plot markers and labels
-%     plotBoneLandmarks(ToesBL, label_switch)   
-
+    plotTriangLight(toesTri, BCS, 0)
+    % % Plot the inertia Axis & Volumic center
+    % quickPlotRefSystem(JCS.(toes_name))
+    quickPlotRefSystem(BCS)
+    % % plot markers and labels
+    % plotBoneLandmarks(ToesBL, label_switch)   
 end
 
 end
