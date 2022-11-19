54b3554f20b7ccc79358caca0b54c20d7a919f49
MCMF <>
2022-11-19 15:59:57 +0100

    reverted fitQuadriTalus.m

--- STAPLE/GIBOC-core/SubFunctions/FittingFun/fitQuadriTalus.m
+++ STAPLE/GIBOC-core/SubFunctions/FittingFun/fitQuadriTalus.m
@@ -17,34 +17,17 @@ if nargin == 2
     plotOn = 0;
 end
 
-%% First project the mesh on the plane perpendicular to X0 (1st inertia axis)
-Pts_inertia = (V_all'*Tr.Points')';
 
-if plotOn
-    figure('name', 'Debug Figure: fitQuadriTalus.m: Projection on X0 (1st inertia axis)', ...
-        'color','w','numbertitle','off');
-    axis equal tight; hold on
-    plot3(Pts_inertia(:,1),Pts_inertia(:,2),Pts_inertia(:,3),'g.')
-    xlabel('X'); ylabel('Y'); zlabel('Z')
-    view(90,0)
-end
+%% First project the mesh on the plan perpendicular to X0 (1st inertia axis)
+YZ0 = [V_all(:,2),V_all(:,3)];
+PX0 = YZ0*inv(YZ0'*YZ0)*YZ0'; %Projection matrix
+Pts_proj = V_all'*PX0*Tr.Points';
 
-% Keep only the 2nd (Y0) and 3rd (Z0) dimension 
-Pts_proj_2D = Pts_inertia(:,2:3);
-assert(sum(sum(Pts_inertia(:,2:3)-Pts_proj_2D)) < 1e-8)
+% Keep only the 2 and 3rd coordinates
+Pts_proj_2D = Pts_proj(2:3,:)';
 
 %Get the convex hull of the point cloud
 K = convhull(Pts_proj_2D,'simplify', false);
-
-if plotOn
-    projFig = figure('name', 'Debug Figure: fitQuadriTalus.m: Convex hull of the projection on X0', ...
-        'color','w','numbertitle','off');
-    projAxes = axes();
-    axis equal tight; hold on
-    plot(Pts_proj_2D(:,1),Pts_proj_2D(:,2),'b.')
-    plot(Pts_proj_2D(K,1),Pts_proj_2D(K,2),'k-')
-    xlabel('Y'); ylabel('Z')
-end
 
 % Find the max edge
 maxDist = -1;
@@ -61,74 +44,51 @@ for i=1:length(K)
     end
 end
 
-% Find the two points of the convex hull with max. distance to each other
-% assuming that it is the 1st diagonal of the quadrilateral
-pwDist = squareform(pdist(Pts_proj_2D(K,:)));
-[~, maxDistIdx] = max(pwDist(:));
-[rowPP1,colPP1] = ind2sub(size(pwDist),maxDistIdx);
-pointPair1 = [K(rowPP1),K(colPP1)];
-U1 = (Pts_proj_2D(pointPair1(2),:)-Pts_proj_2D(pointPair1(1),:))';
-
-if plotOn
-    maxDist1D_H = plot(Pts_proj_2D(pointPair1,1),Pts_proj_2D(pointPair1,2),'r-o');
-    legend(maxDist1D_H, '1st diagonal')
-end
-
 % Hacky way to find the second diagonal of the quadrilateral by shrinking
-% along the point cloud in the direction (U1) of the 1st diagonal found
+% along the point cloud in the direction (U1) of the first found diagonal
 U1 = normalizeV(U1);
 Ru = [U1(1),-U1(2);U1(2),U1(1)];
-Pts_proj_2D_shr = (Ru*[0.5,0;0,1]*Ru'*Pts_proj_2D')';
+Pts_proj_2D_shr = transpose(Ru*[0.5,0;0,1]*Ru'*Pts_proj_2D');
+% plot(Pts_proj_2D_shr(:,1),Pts_proj_2D_shr(:,2),'b.')
 
-if plotOn
-    figure('name', 'Debug Figure: fitQuadriTalus.m: Projection on X0 shrinked along 1st diagonal', ...
-        'color','w','numbertitle','off');
-    axis equal tight; hold on
-    plot(Pts_proj_2D_shr(:,1),Pts_proj_2D_shr(:,2),'g.')
-    line(Pts_proj_2D_shr(K,1),Pts_proj_2D_shr(K,2))
-    xlabel('Y'); ylabel('Z')
+
+% Find the max edge on the shrink points cloud
+maxDist = -1;
+for i=1:length(K)
+    for j=1:length(K)
+        diff1 = Pts_proj_2D_shr(K(j),1)-Pts_proj_2D_shr(K(i),1);
+        diff2 = Pts_proj_2D_shr(K(j),2)-Pts_proj_2D_shr(K(i),2);
+        dist = sqrt(diff1^2+diff2^2);
+        if dist>maxDist
+            maxDist=dist;
+            pointPair2=[K(i),K(j)];
+            U2=[diff1;diff2];
+        end
+    end
 end
 
-% Find the two points of the convex hull with max. distance to each other
-% assuming that it is the 2nd diagonal of the quadrilateral
-pwDist = squareform(pdist(Pts_proj_2D_shr(K,:)));
-[~, maxDistIdx] = max(pwDist(:));
-[rowPP2,colPP2] = ind2sub(size(pwDist),maxDistIdx);
-pointPair2 = [K(rowPP2),K(colPP2)];
-
-if plotOn
-    maxDist2D_H = plot(Pts_proj_2D_shr(pointPair2,1),Pts_proj_2D_shr(pointPair2,2),'r--o');
-    legend(maxDist2D_H, '2nd diagonal')
-end
-
-% Sort the quadrilateral vertices in counter-clockwise direction along the
-% convex hull
-quadriVIdx = sort([rowPP1,colPP1,rowPP2,colPP2]);
-quadriV = K([quadriVIdx,quadriVIdx(1)]);
+% Get the quadrilateral vertices
+quadriV = [pointPair1(1); pointPair2(1);...
+                  pointPair1(2); pointPair2(2);...
+                  pointPair1(1)];
               
-if plotOn
-    maxDist2D_H = plot(projAxes, Pts_proj_2D(pointPair2,1),Pts_proj_2D(pointPair2,2),'r--o');
-    qLitVerts_H = text(projAxes, Pts_proj_2D(quadriV(1:4),1),Pts_proj_2D(quadriV(1:4),2),{'1','2','3','4'},...
-        'HorizontalAlignment','right', 'FontSize',14, 'FontWeight','bold');
-    legend([maxDist1D_H,maxDist2D_H], {'1st diagonal','2nd diagonal'})
-end         
-              
-% Get the length of the edges of the quadrilateral
+% Get the length of the quadri edges 
 edgesLength = zeros(4,1);
 for i=1:4 
     diff1 = Pts_proj_2D(quadriV(i+1),1)-Pts_proj_2D(quadriV(i),1);
     diff2 = Pts_proj_2D(quadriV(i+1),2)-Pts_proj_2D(quadriV(i),2);
     edgesLength(i) = sqrt(diff1^2+diff2^2);
 end
+
 [~,Imax] = max(edgesLength);
 
-% Index of the start vertex of the quadrilateral
+% Indices of start vertices of quadrilateral
 I_V_sup = mod(Imax+2,4);
 % Edge corresponding to the superior part of the bone is assumed to be the
 % one opposing the largest one
 Edge_sup = quadriV(I_V_sup:I_V_sup+1);
 
-% Get the direction of the edge
+% Get the direction of the edge :
 U_edge_sup = [Pts_proj_2D(Edge_sup(2),1)-Pts_proj_2D(Edge_sup(1),1);...
     Pts_proj_2D(Edge_sup(2),2)-Pts_proj_2D(Edge_sup(1),2)];
 %Z0_proj is normal to edge direction
@@ -143,14 +103,17 @@ Z0 = V_all*[0;Z0_proj];
 Y0 = cross(Z0,V_all(:,1));
 
 if plotOn
-    qLitEdge_H = plot(projAxes, Pts_proj_2D(quadriV,1),Pts_proj_2D(quadriV,2),'g-s',...
-        'linewidth',2);
-    supEdge_H = plot(projAxes, Pts_proj_2D(Edge_sup,1),Pts_proj_2D(Edge_sup,2),'m-o',...
-        'linewidth',3);
-    legend([maxDist1D_H,maxDist2D_H,qLitEdge_H,supEdge_H],...
-        {'1st diagonal','2nd diagonal','Quadrilateral edges','Superior edge'})
-    uistack(qLitVerts_H, 'top');
-    figure(projFig)
+    figure()
+    plot(Pts_proj_2D(:,1),Pts_proj_2D(:,2),'b.')
+    hold on
+    plot(Pts_proj_2D(K,1),Pts_proj_2D(K,2),'k-')
+    axis equal
+    plot(Pts_proj_2D(pointPair1,1),Pts_proj_2D(pointPair1,2),'r-*')
+    plot(Pts_proj_2D(pointPair2,1),Pts_proj_2D(pointPair2,2),'r-*')
+    plot(Pts_proj_2D(quadriV,1),Pts_proj_2D(quadriV,2),'g-s',...
+        'linewidth',2)
+    plot(Pts_proj_2D(Edge_sup,1),Pts_proj_2D(Edge_sup,2),'m-o',...
+        'linewidth',3)
 end
 
 end
